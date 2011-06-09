48e1acd8bd2cf492845af8df5707ab9f085da9e4
Stephen Early <sde@individualpubs.co.uk>
2011-06-09 18:10:55 +0100

    Add transaction_notes configuration option
    transaction_notes should be a list of strings.  The empty string should
    probably be included, to enable transaction notes to be removed!

--- quicktill/till.py
+++ quicktill/till.py
@@ -168,6 +168,8 @@ def main():
         tillconfig.checkdigit_on_usestock=config['checkdigit_on_usestock']
     if 'allow_tabs' in config:
         tillconfig.allow_tabs=config['allow_tabs']
+    if 'transaction_notes' in config:
+        tillconfig.transaction_notes=config['transaction_notes']
 
     if os.uname()[0]=='Linux':
         if os.getenv('TERM')=='xterm': os.putenv('TERM','linux')
