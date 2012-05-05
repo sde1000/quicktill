c897384abf2bb60d57055f648e29da33d9b93447
Stephen Early <sde@individualpubs.co.uk>
2012-05-05 15:42:33 +0100

    Change default admin site address

--- quicktill/timesheets.py
+++ quicktill/timesheets.py
@@ -54,7 +54,7 @@ class Api(object):
     '''
     def __init__(self,
                  username,password,site=None,base_url=None):
-        if site is None: site="https://secure.individualpubs.co.uk"
+        if site is None: site="https://admin.individualpubs.co.uk"
         if base_url is None:
             base_url="/schedule/%s/api/users/"%username
         self._site=site
