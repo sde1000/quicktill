6bf7a52b47b9ba8376eeb1678566e57c2bdec533
Stephen Early <sde@individualpubs.co.uk>
2013-06-29 10:25:07 +0100

    quicktill 0.9.30

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='quicktill',
-      version='0.9.29',
+      version='0.9.30',
       description='Quick till and stock control library',
       author='Stephen Early',
       author_email='steve@greenend.org.uk',
