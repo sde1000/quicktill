5c6ab68d2750f577f7505421fe33adc65bdd5269
Stephen Early <sde@individualpubs.co.uk>
2013-09-12 14:42:10 +0100

    Make sure department lists are sorted

--- quicktill/department.py
+++ quicktill/department.py
@@ -2,7 +2,7 @@ from . import td,ui
 from .models import Department
 
 def menu(func,title,allowall=False):
-    depts=td.s.query(Department).all()
+    depts=td.s.query(Department).order_by(Department.id).all()
     lines=ui.table([("%d"%d.id,d.description) for d in depts]).format(' r l ')
     sl=[(x,func,(y.id,)) for x,y in zip(lines,depts)]
     if allowall:
