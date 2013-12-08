c73142acfc1cd166f89896f618211bb4b91f5f8f
Stephen Early <steve@greenend.org.uk>
2013-12-08 14:27:51 +0000

    WIP: new stock item picker
    New stock item picker is used for annotations and record waste
    
    It still needs shortcuts adding for line keys

--- quicktill/recordwaste.py
+++ quicktill/recordwaste.py
@@ -38,120 +38,42 @@ def stockline_chosen(stockline):
 
 class record_item_waste(ui.dismisspopup):
     """
-    This popup talks the user through the process of recording waste
-    against a stock item.  If the stock item is on sale on a "display"
-    type stockline, waste is recorded against the amount still in
-    stock in that particular item; otherwise it is recorded against
-    the item as a whole.  A series of prompts are issued; the Clear
-    key will kill the whole window and will not allow backtracking.
-
+    This popup enables the user to record waste against a single stock
+    item.
+    
     """
     def __init__(self,stockitem=None):
         ui.dismisspopup.__init__(self,10,70,title="Record Waste",
                                  colour=ui.colour_input)
         self.addstr(2,2,"Press stock line key or enter stock number.")
-        self.addstr(3,2,"       Stock item:")
-        stockfield_km={keyboard.K_CLEAR: (self.dismiss,None),
-                       keyboard.K_CASH: (self.stock_enter_key,None)}
-        self.stockfield=ui.editfield(3,21,30,validate=ui.validate_int)
-        if stockitem: self.stockfield.set(stockitem.id)
-        self.stockfield.focus()
-    def stock_line(self,kb):
-        td.s.add(kb)
-        line=kb.stockline
-        if line.capacity is None:
-            if len(line.stockonsale)==0:
-                ui.infopopup(["There is nothing on sale on %s."%line.name],
-                             title="Error")
-            else:
-                self.stockfield.set(str(line.stockonsale[0].id))
-                self.stock_enter_key()
-            return
-        self.isline=True
-        self.stocklineid=line.id
-        self.name=line.name
-        self.ondisplay=line.ondisplay
-        if self.ondisplay<1:
-            self.dismiss()
-            ui.infopopup(
-                ["There is no stock on display for '%s'.  If you want to "
-                 "record waste against items still in storage, you have "
-                 "to enter the stock number instead of pressing the line "
-                 "key."%line.name],title="No stock on display")
-            return
-        self.stockfield.set(line.name)
-        self.addstr(4,21,"%d items on display"%self.ondisplay)
-        self.create_extra_fields()
-    def stock_dept_selected(self,dept):
-        sinfo=td.s.query(StockItem).join(StockItem.stocktype).\
-            filter(StockItem.finished==None).\
-            filter(StockType.dept_id==dept).\
-            order_by(StockItem.id).\
-            all()
-        f=ui.tableformatter(' r l ')
-        sl=[(ui.tableline(f,(x.id,x.stocktype.format())),
-             self.stock_item_selected,(x.id,)) for x in sinfo]
-        ui.menu(sl,title="Select Item",blurb="Select a stock item and press "
-                "Cash/Enter.")
-    def stock_item_selected(self,stockid):
-        self.stockfield.set(str(stockid))
-        self.stock_enter_key()
-    def stock_enter_key(self):
-        if self.stockfield.f=='':
-            department.menu(self.stock_dept_selected,"Select Department")
-            return
-        sn=int(self.stockfield.f)
-        sd=td.s.query(StockItem).get(sn)
-        if sd is None:
-            ui.infopopup(["Stock number %d does not exist."%sn],
-                         title="Error")
-            return
-        if not sd.delivery.checked:
-            ui.infopopup(["Stock number %d is part of a delivery that has "
-                          "not yet been confirmed.  You can't record waste "
-                          "against it until the whole delivery is confirmed."%(
-                        sn)],
-                         title="Error")
-            return
-        self.isline=False
-        self.sd=sd
-        self.addstr(4,21,sd.stocktype.format(maxw=40))
-        self.create_extra_fields()
-    def create_extra_fields(self):
+        self.addstr(4,2,"       Stock item:")
         self.addstr(5,2,"Waste description:")
         self.addstr(6,2,"    Amount wasted:")
-        # XXX all of this needs to be re-done.  We should call a hook
-        # from the till config to obtain a suitable list of RemoveCode
-        # models.
-        #if self.isline:
-        #    wastelist=['missing','taste','damaged','ood','freebie']
-        #else:
-        #    wastelist=['cellar','pullthru','taster','taste','damaged',
-        #               'ood','freebie','missing','driptray']
-        #wastedict={'pullthru':'Pulled through',
-        #           'cellar':'Cellar work',
-        #           'taster':'Free taster',
-        #           'taste':'Bad taste',
-        #           'damaged':'Damaged',
-        #           'ood':'Out of date',
-        #           'freebie':'Free drink',
-        #           'missing':'Gone missing',
-        #           'driptray':'Drip tray'}
+        self.stockfield=stock.stockfield(4,21,47,keymap={
+                keyboard.K_CLEAR: (self.dismiss,None),},
+                                         check_checkdigits=True)
+        self.stockfield.sethook=self.stockfield_updated
         wastelist=td.s.query(RemoveCode).filter(RemoveCode.id!='sold').all()
         self.wastedescfield=ui.listfield(
-            5,21,30,wastelist,lambda rc:rc.reason,
-            keymap={keyboard.K_CLEAR:(self.dismiss,None)})
+            5,21,30,wastelist,lambda rc:rc.reason)
         self.amountfield=ui.editfield(
             6,21,4,validate=ui.validate_float,
             keymap={keyboard.K_CASH: (self.finish,None)})
-        ui.map_fieldlist([self.wastedescfield,self.amountfield])
-        if self.isline:
-            self.addstr(6,26,'items')
+        ui.map_fieldlist([self.stockfield,self.wastedescfield,self.amountfield])
+        if stockitem:
+            self.stockfield.set(stockitem)
+            self.wastedescfield.focus()
         else:
-            self.addstr(6,26,self.sd.stocktype.unit.name+'s')
-        self.wastedescfield.set(wastelist[0])
-        self.wastedescfield.focus()
+            self.stockfield.focus()
+    def stockfield_updated(self):
+        self.addstr(6,26," "*20)
+        s=self.stockfield.read()
+        if s: self.addstr(6,26,u"%ss"%s.stocktype.unit.name)
     def finish(self):
+        item=self.stockfield.read()
+        if item is None:
+            ui.infopopup(["You must choose a stock item."],title="Error")
+            return
         waste=self.wastedescfield.read()
         if waste is None or waste=="":
             ui.infopopup(["You must enter a waste description!"],title="Error")
@@ -165,37 +87,19 @@ class record_item_waste(ui.dismisspopup):
                          title='Error')
             self.amountfield.set("")
             return
-        if self.isline:
-            amount=int(amount)
-            if amount>self.ondisplay:
-                ui.infopopup(["You asked to record waste of %d items, but "
-                              "there are only %d on display."%(
-                    amount,self.ondisplay)],
-                             title="Error")
-                return
-            sell,unallocated,remaining=stocklines.calculate_sale(
-                self.stocklineid,amount)
-            for item,qty in sell:
-                td.s.add(StockOut(stockitem=item,qty=qty,removecode=waste))
-            td.s.flush()
-            self.dismiss()
-            ui.infopopup(["Recorded %d items against stock line %s."%(
-                amount,self.name)],title="Waste Recorded",
-                         dismiss=keyboard.K_CASH,colour=ui.colour_info)
-        else:
-            td.s.add(self.sd)
-            td.s.add(StockOut(stockitem=self.sd,qty=amount,removecode=waste))
-            # If this is an item on display, we increase displayqty by
-            # the amount wasted
-            if self.sd.stockline and self.sd.stockline.capacity:
-                self.sd.displayqty=self.sd.displayqty_or_zero+int(amount)
-            td.s.flush()
-            self.dismiss()
-            ui.infopopup(["Recorded %0.1f %ss against stock item %d (%s)."%(
-                amount,self.sd.stocktype.unit.name,self.sd.id,
-                self.sd.stocktype.format())],
-                         title="Waste Recorded",dismiss=keyboard.K_CASH,
-                         colour=ui.colour_info)
+        td.s.add(item)
+        td.s.add(StockOut(stockitem=item,qty=amount,removecode=waste))
+        # If this is an item on display, we increase displayqty by
+        # the amount wasted
+        if item.stockline and item.stockline.capacity:
+            item.displayqty=item.displayqty_or_zero+int(amount)
+        td.s.flush()
+        self.dismiss()
+        ui.infopopup(["Recorded %0.1f %ss against stock item %d (%s)."%(
+                    amount,item.stocktype.unit.name,item.id,
+                    item.stocktype.format())],
+                     title="Waste Recorded",dismiss=keyboard.K_CASH,
+                     colour=ui.colour_info)
 
 class record_line_waste(ui.dismisspopup):
     """
--- quicktill/stock.py
+++ quicktill/stock.py
@@ -6,7 +6,7 @@ import logging
 from decimal import Decimal
 from . import ui,td,keyboard,tillconfig,stocklines,department
 from .models import Department,StockType,StockItem,StockAnnotation
-from .models import AnnotationType
+from .models import AnnotationType,Delivery,desc
 log=logging.getLogger(__name__)
 
 # XXX should probably convert this to take a StockItem object rather
@@ -47,97 +47,42 @@ def stockinfo_popup(sn,keymap={}):
                  colour=ui.colour_info,keymap=keymap)
 
 class annotate(ui.dismisspopup):
-    """This class permits annotations to be made to stock items.  If
-    it is called with a stockid then the stockid field is pre-filled;
-    otherwise a numeric entry may be made, or a pop-up list may be
-    used to select the stockid.
+    """
+    This class permits annotations to be made to stock items.
 
     """
-    def __init__(self,stockid=None):
-        ui.dismisspopup.__init__(self,11,64,"Annotate Stock",
+    def __init__(self):
+        ui.dismisspopup.__init__(self,9,70,"Annotate Stock",
                                  colour=ui.colour_input)
         self.addstr(2,2,"Press stock line key or enter stock number.")
         self.addstr(3,2,"       Stock item:")
-        stockfield_km={keyboard.K_CLEAR: (self.dismiss,None),
-                       keyboard.K_CASH: (self.stock_enter_key,None)}
-        # XXX this requires updating after the keyboard change.
-        # Suggest a new type of field for choosing a stock item.
-        for i in keyboard.lines:
-            stockfield_km[i]=(stocklines.linemenu,(i,self.stock_line))
-        self.stockfield=ui.editfield(3,21,30,validate=ui.validate_int,
-                                     keymap=stockfield_km)
-        self.stockfield.focus()
-        if stockid is not None:
-            self.stockfield.set(str(stockid))
-            self.stock_enter_key()
-    def stock_line(self,line):
-        td.s.add(line)
-        if line.capacity is None:
-            # Look up the stock number, put it in the field, and invoke
-            # stock_enter_key
-            sl=line.stockonsale
-            if len(sl)==0:
-                ui.infopopup(["There is nothing on sale on %s."%line.name],
-                             title="Error")
-            else:
-                self.stockfield.set(str(sl[0].id))
-                self.stock_enter_key()
-    def stock_dept_selected(self,dept):
-        sinfo=td.s.query(StockItem).join(StockItem.stocktype).\
-            filter(StockItem.finished==None).\
-            filter(StockType.dept_id==dept).\
-            order_by(StockItem.id).\
-            all()
-        f=ui.tableformatter(' r l ')
-        sl=[(ui.tableline(f,(x.id,x.stocktype.format())),
-             self.stock_item_selected,(x.id,)) for x in sinfo]
-        ui.menu(sl,title="Select Item",blurb="Select a stock item and press "
-                "Cash/Enter.")
-    def stock_item_selected(self,stockid):
-        self.stockfield.set(str(stockid))
-        self.stock_enter_key()
-    def stock_enter_key(self):
-        if self.stockfield.f=='':
-            department.menu(self.stock_dept_selected,"Select Department")
-            return
-        sn=int(self.stockfield.f)
-        sd=td.s.query(StockItem).get(sn)
-        if sd is None:
-            ui.infopopup(["Stock number %d does not exist."%sn],
-                         title="Error")
-            return
-        if not sd.delivery.checked:
-            ui.infopopup(["Stock number %d is part of a delivery that has "
-                          "not yet been confirmed.  You can't annotate "
-                          "it until the whole delivery is confirmed."%(sd.id)],
-                         title="Error")
-            return
-        # We want the popup to be focused while creating the next two
-        # fields; if the stocknumber field is focused then it will be
-        # their parent and focus may return there in error!
-        self.focus()
-        self.sn=sn
-        self.addstr(4,21,sd.stocktype.format(maxw=40))
-        self.addstr(5,2,"Annotation type:")
-        self.addstr(7,2,"Annotation:")
+        stockfield_km={keyboard.K_CLEAR: (self.dismiss,None),}
+        self.stockfield=stockfield(3,21,47,keymap=stockfield_km,
+                                   filter=stockfilter(allow_finished=True),
+                                   title="Choose stock item to annotate",
+                                   check_checkdigits=False)
+        self.addstr(4,2,"Annotation type:")
         annlist=td.s.query(AnnotationType).\
             filter(~AnnotationType.id.in_(['start','stop'])).\
             all()
-        self.anntypefield=ui.listfield(5,21,30,annlist,
-                                       d=lambda x:x.description,keymap={
-                keyboard.K_CLEAR:(self.dismiss,None)})
-        self.annfield=ui.editfield(8,2,60,keymap={
+        self.anntypefield=ui.listfield(
+            4,21,30,annlist,d=lambda x:x.description)
+        self.addstr(5,2,"Annotation:")
+        self.annfield=ui.editfield(6,2,60,keymap={
                 keyboard.K_CASH: (self.finish,None)})
-        ui.map_fieldlist([self.anntypefield,self.annfield])
-        self.anntypefield.focus()
+        ui.map_fieldlist([self.stockfield,self.anntypefield,self.annfield])
+        self.stockfield.focus()
+        ui.handle_keyboard_input(keyboard.K_CASH) # Pop up picker
     def finish(self):
+        item=self.stockfield.read()
+        if item is None:
+            ui.infopopup(["You must choose a stock item."],title="Error")
+            return
         anntype=self.anntypefield.read()
         if anntype is None:
             ui.infopopup(["You must choose an annotation type!"],title="Error")
             return
         annotation=self.annfield.f or ""
-        item=td.s.query(StockItem).get(self.sn)
-        log.debug("%s %s %s",item,anntype,annotation)
         td.s.add(
             StockAnnotation(stockitem=item,type=anntype,text=annotation))
         td.s.flush()
@@ -181,3 +126,194 @@ class annotate_location(ui.dismisspopup):
         td.s.flush()
         self.dismiss()
 
+class stockfilter(object):
+    """
+    Filter and sort available stock items according to various
+    criteria.  Stock items that are in unchecked deliveries can never
+    be returned.
+
+    """
+    def __init__(self,department=None,allow_on_sale=True,allow_finished=False,
+                 stockline_affinity=None,sort_descending_stockid=False):
+        self.department=department
+        self.allow_on_sale=allow_on_sale
+        self.allow_finished=allow_finished
+        self.stockline_affinity=stockline_affinity
+        self.sort_descending_stockid=sort_descending_stockid
+    @property
+    def is_single_department(self):
+        return self.department is not None
+    def query_items(self,department=None):
+        """
+        Return a query that lists matching items.  If a department is
+        passed, this overrides the department restriction in the
+        object.
+
+        """
+        if department is None: department=self.department
+        q=td.s.query(StockItem).join(Delivery).filter(Delivery.checked==True)
+        if department:
+            td.s.add(department)
+            q=q.join(StockType)
+            q=q.filter(StockType.department==department)
+        if not self.allow_on_sale:
+            q=q.filter(StockItem.stocklineid==None)
+        if not self.allow_finished:
+            q=q.filter(StockItem.finished==None)
+        if self.stockline_affinity:
+            td.s.add(self.stockline_affinity)
+            # XXX add order by stockline affinity here
+        if self.sort_descending_stockid:
+            q=q.order_by(desc(StockItem.id))
+        else:
+            q=q.order_by(StockItem.id)
+        return q
+    def item_problem(self,item):
+        """
+        If the passed item matches the criteria, returns None.
+        Otherwise returns a string describing at least one problem
+        with the item.  The item is expected to be attached to an ORM
+        session.
+
+        """
+        if not item.delivery.checked: return "delivery not checked"
+        if self.department:
+            d=td.s.merge(self.department)
+            if item.stocktype.department!=d: return "wrong department"
+        if item.stocklineid is not None and not self.allow_on_sale:
+            return u"already on sale on %s"%item.stockline.name
+        if item.finished is not None and not self.allow_finished:
+            return u"finished at %s"%item.finished
+
+class stockpicker(ui.dismisspopup):
+    """
+    A popup window that allows a stock item to be chosen.  Contains
+    one field for the stock number, and optionally another field for
+    the check digits.  Pressing Enter on a blank stock number field
+    will yield a popup list of departments (if no department
+    constraint is specified) leading to a popup list of stock items.
+
+    If a StockLine is specified for stockline_affinity then items of
+    stock that have previously been on sale on that StockLine will be
+    sorted to the top of the list.
+
+    When the StockItem has been chosen, func is called with it as an
+    argument.
+
+    """
+    def __init__(self,func,default=None,title="Choose stock item",
+                 filter=stockfilter(),check_checkdigits=True):
+        if default: td.s.add(default)
+        self.title=title
+        self.func=func
+        self.filter=filter
+        self.check=check_checkdigits and tillconfig.checkdigit_on_usestock
+        h=9 if self.check else 7
+        ui.dismisspopup.__init__(self,h,60,title,colour=ui.colour_input)
+        self.addstr(2,2,"   Stock ID:")
+        self.numfield=ui.editfield(
+            2,15,8,validate=ui.validate_int,f=default.id if default else None,
+            keymap={keyboard.K_CASH:(self.numfield_enter,None),
+                    keyboard.K_DOWN:(self.numfield_enter,None)})
+        self.numfield.sethook=self.numfield_set
+        if self.check:
+            self.addstr(6,2,"Checkdigits:")
+            self.checkfield=ui.editfield(
+                6,15,3,validate=ui.validate_int,
+                keymap={keyboard.K_CASH:(self.checkfield_enter,None),
+                        keyboard.K_CLEAR:(self.numfield.focus,None),
+                        keyboard.K_UP:(self.numfield.focus,None)})
+            self.checkfield.sethook=self.checkfield_set
+        self.numfield.focus()
+    def numfield_set(self):
+        self.addstr(3,15," "*43)
+        self.addstr(4,15," "*43)
+        if self.numfield.f:
+            stockid=int(self.numfield.f)
+            item=td.s.query(StockItem).get(stockid)
+            if item:
+                self.addstr(3,15,item.stocktype.format(maxw=43))
+                not_ok=self.filter.item_problem(item)
+                if not_ok: self.addstr(4,15,u"(%s)"%not_ok)
+    def checkfield_set(self):
+        self.addstr(6,20," "*7)
+        if len(self.checkfield.f)!=3: return
+        stockid=int(self.numfield.f)
+        item=td.s.query(StockItem).get(stockid)
+        self.addstr(6,20,"(OK)" if item and item.checkdigits==self.checkfield.f
+                    else "(wrong)")
+    def numfield_enter(self):
+        if self.numfield.f:
+            # Only advance if there's a valid stock item there
+            stockid=int(self.numfield.f)
+            item=self.filter.query_items().\
+                filter(StockItem.id==stockid).first()
+            if item:
+                if self.check:
+                    self.checkfield.focus()
+                else:
+                    self.dismiss()
+                    self.func(item)
+        else:
+            if self.filter.is_single_department:
+                self.popup_menu(None)
+            else:
+                depts=td.s.query(Department).order_by(Department.id).all()
+                f=ui.tableformatter(' r l ')
+                lines=[(ui.tableline(f,(d.id,d.description)),
+                        self.popup_menu,(d,)) for d in depts]
+                ui.menu(lines)
+    def popup_menu(self,department):
+        items=self.filter.query_items(department).all()
+        f=ui.tableformatter(' r l c ')
+        sl=[(ui.tableline(f,(s.id,s.stocktype.format(),"%s %ss"%(
+                            s.remaining,s.stockunit.unit.name))),
+             self.item_chosen,(s.id,))
+            for s in items]
+        ui.menu(sl,title=self.title)
+    def item_chosen(self,stockid):
+        # An item has been chosen from the popup menu
+        self.numfield.set(str(stockid))
+        self.numfield_enter()
+    def checkfield_enter(self):
+        stockid=int(self.numfield.f)
+        item=td.s.query(StockItem).get(stockid)
+        if self.checkfield.f==item.checkdigits:
+            self.dismiss()
+            self.func(item)
+
+class stockfield(ui.popupfield):
+    """
+    A field that allows a stock item to be chosen.  If
+    check_checkdigits is True then manual entry of stock numbers will
+    result in a checkdigits check; choosing stock items on sale on a
+    stock line will skip the check.
+
+    """
+    def __init__(self,y,x,w,f=None,keymap={},readonly=False,
+                 filter=stockfilter(),check_checkdigits=True,
+                 title="Choose stock item"):
+        ui.popupfield.__init__(
+            self,y,x,w,popupfunc=stockpicker, # Pass on args?
+            valuefunc=lambda x:u"%d: %s"%(
+                x.id,x.stocktype.format(maxw=w-2-len(str(x.id)))),
+            f=f,readonly=readonly,keymap=keymap)
+        self.filter=filter
+        self.check_checkdigits=check_checkdigits
+        self.title=title
+    def popup(self):
+        # If we're popping up the stockpicker, it's because we want to
+        # pick a different stock item!  Setting the current one as
+        # default doesn't make sense - the user would just have to
+        # press 'Clear' to get rid of it.
+        if not self.readonly:
+            stockpicker(self.setf,filter=self.filter,
+                        check_checkdigits=self.check_checkdigits,
+                        title=self.title)
+    def keypress(self,k):
+        if k in keyboard.numberkeys and not self.readonly:
+            # Pass on the keypress to the stocknumber entry popup
+            self.popup()
+            ui.handle_keyboard_input(k)
+        else:
+            ui.popupfield.keypress(self,k)
