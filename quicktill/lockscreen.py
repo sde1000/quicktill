7a6b02db3dfdbe4e32c79659d4b63fa3d7a6cd2c
Stephen Early <sde@individualpubs.co.uk>
2014-02-01 13:53:34 +0000

    Show version on lock screen

--- quicktill/lockscreen.py
+++ quicktill/lockscreen.py
@@ -1,19 +1,20 @@
 from __future__ import unicode_literals
-from . import ui
+from . import ui,version
 
 class lockpage(ui.basicpage):
     def __init__(self):
         ui.basicpage.__init__(self)
-        self.win.addstr(1,1,"This till is locked.")
+        self.addstr(1,1,"This till is locked.")
         self.updateheader()
         unsaved=[p for p in ui.basicpage._pagelist if p!=self]
         if unsaved:
-            self.win.addstr(3,1,"The following users have unsaved work "
+            self.addstr(3,1,"The following users have unsaved work "
                             "on this terminal:")
             y=4
             for p in unsaved:
                 self.win.addstr(y,3,p.pagename())
                 y=y+1
+        self.addstr(self.h-1,0,"Till version: {}".format(version.version))
         self.win.move(0,0)
     def pagename(self):
         return "Lock"
