bc8295737da842f993d1e6a67f8dc165c4a45f80
Stephen Early <sde@individualpubs.co.uk>
2014-02-04 19:07:11 +0000

    quicktill 0.10.16

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from distutils.core import setup
 
 setup(name='quicktill',
-      version='0.10.15',
+      version='0.10.16',
       description='Quick till and stock control library',
       author='Stephen Early',
       author_email='steve@greenend.org.uk',
       url='https://github.com/sde1000/quicktill',
       packages=['quicktill','quicktill.tillweb'],
       package_data={'quicktill.tillweb':
-                        ['static/tillweb/*.js','templates/tillweb/*.html']},
+                        ['static/tillweb/*.js','templates/tillweb/*.html',
+                         'static/tillweb/*.css','static/tillweb/images/*']},
       scripts=['runtill'],
       )
