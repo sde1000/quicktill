a08c01685bad908e2fe6c110da31af7e29120ece
Stephen Early <steve@greenend.org.uk>
2014-09-28 17:40:50 +0100

    Update department.py tableformatter calls

--- quicktill/department.py
+++ quicktill/department.py
@@ -5,8 +5,7 @@ from .models import Department
 def menu(func,title,allowall=False):
     depts=td.s.query(Department).order_by(Department.id).all()
     f=ui.tableformatter(' r l ')
-    lines=[(ui.tableline(f,(d.id,d.description)),func,(d.id,))
-           for d in depts]
+    lines=[(f(d.id,d.description),func,(d.id,)) for d in depts]
     if allowall:
         lines.insert(0,("All departments",func,(None,)))
     ui.menu(lines,title=title,blurb="Choose a department and press Cash/Enter.")
