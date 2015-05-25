8d0dd20553d5fa18272acb689395878a69c25c6b
Stephen Early <sde@individualpubs.co.uk>
2015-05-25 11:14:06 +0100

    Remove list of modules from init file
    It's never useful to do from quicktill import *, so remove the list of
    modules from the file.

--- quicktill/__init__.py
+++ quicktill/__init__.py
@@ -4,13 +4,3 @@ A library for creating a variety of flexible cash register and stock
 control system user interfaces with a PostgreSQL back-end.
 
 """
-
-__all__=['bitcoin','card','cash','cmdline','dbsetup','delivery','department',
-         'event','extras',
-         'foodcheck','foodorder','kbdrivers','keyboard','lockscreen',
-         'managekeyboard','managestock','managetill','models',
-         'payment',
-         'pdrivers','plu','printer','recordwaste','register',
-         'session','stocklines',
-         'stock','stockterminal','stocktype','td','tillconfig','till',
-         'timesheets','ui','user','usestock','version']
