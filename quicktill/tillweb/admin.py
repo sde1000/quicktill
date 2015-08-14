cddc2e0d9f94616fe8684d3c49890e771230b1e0
Stephen Early <steve@greenend.org.uk>
2015-08-14 10:22:08 +0100

    Add list filter to tillweb access admin

--- quicktill/tillweb/admin.py
+++ quicktill/tillweb/admin.py
@@ -1,5 +1,9 @@
 from .models import *
 from django.contrib import admin
 
+class AccessAdmin(admin.ModelAdmin):
+    list_filter=('till',)
+    list_display=('user','till')
+
 admin.site.register(Till)
-admin.site.register(Access)
+admin.site.register(Access,AccessAdmin)
