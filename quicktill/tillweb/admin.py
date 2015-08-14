e6ac408a84792e891d17e58c17106fe246af81c0
Stephen Early <steve@greenend.org.uk>
2015-08-14 10:37:07 +0100

    Add user full name to list of accesses in tillweb

--- quicktill/tillweb/admin.py
+++ quicktill/tillweb/admin.py
@@ -1,9 +1,13 @@
 from .models import *
 from django.contrib import admin
 
+def user_full_name(access):
+    return access.user.get_full_name()
+user_full_name.short_description="Name"
+
 class AccessAdmin(admin.ModelAdmin):
     list_filter=('till',)
-    list_display=('user','till')
+    list_display=('user',user_full_name,'till')
 
 admin.site.register(Till)
 admin.site.register(Access,AccessAdmin)
