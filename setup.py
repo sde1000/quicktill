b5f85515c58147400cf4b7689bf25614a69b8c8e
Stephen Early <sde@individualpubs.co.uk>
2015-11-17 16:39:43 +0000

    Add .ajax templates to package

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='quicktill',
       packages=['quicktill','quicktill.tillweb'],
       package_data={'quicktill.tillweb':
                         ['static/tillweb/*.js','templates/tillweb/*.html',
+                         'templates/tillweb/*.ajax',
                          'static/tillweb/*.css','static/tillweb/images/*']},
       scripts=['runtill'],
       )
