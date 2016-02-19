9c5e7ba8ab7c971637759ca154ea87ce1231823d
Stephen Early <steve@greenend.org.uk>
2016-02-19 17:39:39 +0000

    Treat default version string as utf-8

--- quicktill/version.py
+++ quicktill/version.py
@@ -4,7 +4,8 @@
 import subprocess
 
 try:
-    version = subprocess.check_output(['git','describe','--dirty']).strip()
+    version = subprocess.check_output(['git','describe','--dirty']).\
+              strip().decode('utf-8')
 except:
     version = ("unknown (not released, and either not in revision control or "
                "the current working directory is not in the quicktill tree)")
