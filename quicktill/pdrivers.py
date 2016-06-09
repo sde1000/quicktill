93e7d9913ac2d5897335976520934378f51495fe
Stephen Early <steve@greenend.org.uk>
2016-06-09 12:27:24 +0100

    Fix QR code printing
    NB now the printqrcode() method must be called with bytes() as the
    parameter.

--- quicktill/pdrivers.py
+++ quicktill/pdrivers.py
@@ -65,9 +65,6 @@ def test_ping(host):
     if r==0: return True
     return False
 
-def l2s(l):
-    return bytes(l)
-
 def lrwrap(l,r,width):
     w=textwrap.wrap(l,width)
     if len(w)==0: w=[""]
@@ -200,8 +197,8 @@ class linux_lpprinter(fileprinter):
         if self._file: return 
         try:
             f = open(self._getfilename(), 'ab')
-            buf=array.array(str('b'),[0])
-            fcntl.ioctl(f,self.LPGETSTATUS,buf)
+            buf = array.array('b', [0])
+            fcntl.ioctl(f, self.LPGETSTATUS, buf)
             f.close()
             return self._decode_status(buf[0])
         except IOError as e:
@@ -337,104 +334,99 @@ class cupsprinter(tmpfileprinter):
                        self._options)
 
 def ep_2d_cmd(*params):
-    """
-    Assemble an ESC/POS 2d barcode command.  params are either 8-bit
-    integers or strings, which will be concatenated.  Strings are
-    assumed to be sequences of bytes here!
+    """Assemble an ESC/POS 2d barcode command.
 
+    params are either 8-bit integers or bytes(), which will be
+    concatenated.
     """
-    p = bytes(params)
+    p = b''.join([bytes([x]) if isinstance(x, int) else x for x in params])
     pL = len(p) & 0xff
     pH = (len(p) >> 8) & 0xff
     return bytes([29, ord('('), ord('k'), pL, pH]) + p
 
 class escpos(object):
+    """The ESC/POS protocol for controlling receipt printers.
     """
-    The ESC/POS protocol for controlling receipt printers.
-
-    """
-    filesuffix=".dat"
-    ep_reset=l2s([27,64,27,116,16])
-    ep_pulse=l2s([27,ord('p'),0,50,50])
-    ep_underline=(l2s([27,45,0]),l2s([27,45,1]),l2s([27,45,2]))
-    ep_emph=(l2s([27,69,0]),l2s([27,69,1]))
-    ep_colour=(l2s([27,114,0]),l2s([27,114,1]))
-    ep_font=(l2s([27,77,0]),l2s([27,77,1]))
-    ep_left=l2s([27,97,0])
-    ep_center=l2s([27,97,1])
-    ep_right=l2s([27,97,2])
-    ep_ff=l2s([27,100,7])
-    ep_fullcut=l2s([27,105])
-    ep_unidirectional_on=l2s([27,85,1])
-    ep_unidirectional_off=l2s([27,85,0])
-    ep_bitimage_sd=l2s([27,42,0]) # follow with 16-bit little-endian data length
-    ep_short_feed=l2s([27,74,5])
-    ep_half_dot_feed=l2s([27,74,1])
-    def __init__(self,cpl,dpl,coding,has_cutter=False,
-                 lines_before_cut=3,default_font=0,
+    filesuffix = ".dat"
+    ep_reset = bytes([27, 64, 27, 116, 16])
+    ep_pulse = bytes([27, ord('p'), 0, 50, 50])
+    ep_underline = (bytes([27, 45, 0]), bytes([27, 45, 1]), bytes([27, 45, 2]))
+    ep_emph = (bytes([27, 69, 0]), bytes([27, 69, 1]))
+    ep_colour = (bytes([27, 114, 0]), bytes([27, 114, 1]))
+    ep_font = (bytes([27, 77, 0]), bytes([27, 77, 1]))
+    ep_left = bytes([27, 97, 0])
+    ep_center = bytes([27, 97, 1])
+    ep_right = bytes([27, 97, 2])
+    ep_ff = bytes([27, 100, 7])
+    ep_fullcut = bytes([27, 105])
+    ep_unidirectional_on = bytes([27, 85, 1])
+    ep_unidirectional_off = bytes([27, 85, 0])
+    ep_bitimage_sd = bytes([27, 42, 0]) # follow with 16-bit little-endian data length
+    ep_short_feed = bytes([27, 74, 5])
+    ep_half_dot_feed = bytes([27, 74, 1])
+    def __init__(self, cpl, dpl, coding, has_cutter=False,
+                 lines_before_cut=3, default_font=0,
                  native_qrcode_support=False):
-        self.f=None
-        self.fontcpl=cpl
-        self.dpl=dpl
-        self.coding=coding
-        self.has_cutter=has_cutter
-        self.lines_before_cut=lines_before_cut
-        self.default_font=default_font
-        self.native_qrcode_support=native_qrcode_support
-    def start(self,fileobj,interface):
-        self.f=fileobj
-        self.colour=0
-        self.font=self.default_font
-        self.emph=0
-        self.underline=0
-        self.cpl=self.fontcpl[0]
+        self.f = None
+        self.fontcpl = cpl
+        self.dpl = dpl
+        self.coding = coding
+        self.has_cutter = has_cutter
+        self.lines_before_cut = lines_before_cut
+        self.default_font = default_font
+        self.native_qrcode_support = native_qrcode_support
+    def start(self, fileobj, interface):
+        self.f = fileobj
+        self.colour = 0
+        self.font = self.default_font
+        self.emph = 0
+        self.underline = 0
+        self.cpl = self.fontcpl[self.font]
         self.f.write(escpos.ep_reset)
         self.f.write(escpos.ep_font[self.font])
-        self._printed=False
+        self._printed = False
         return self
     def end(self):
         if self._printed:
             self.f.write(escpos.ep_ff)
             if self.has_cutter:
-                self.f.write(b'\n'*self.lines_before_cut+escpos.ep_left)
+                self.f.write(b'\n' * self.lines_before_cut + escpos.ep_left)
                 self.f.write(escpos.ep_fullcut)
             self.f.flush()
-        self.f=None
-        self._printed=False
-    def setdefattr(self,colour=None,font=None,emph=None,underline=None):
+        self.f = None
+    def setdefattr(self, colour=None, font=None, emph=None, underline=None):
         if colour is not None:
-            if colour!=self.colour:
-                self.colour=colour
+            if colour != self.colour:
+                self.colour = colour
                 self.f.write(escpos.ep_colour[colour])
         if font is not None:
-            if font!=self.font:
-                self.font=font
-                self.cpl=self.fontcpl[font]
+            if font != self.font:
+                self.font = font
+                self.cpl = self.fontcpl[font]
                 self.f.write(escpos.ep_font[font])
         if emph is not None:
-            if emph!=self.emph:
-                self.emph=emph
+            if emph != self.emph:
+                self.emph = emph
                 self.f.write(escpos.ep_emph[emph])
         if underline is not None:
-            if underline!=self.underline:
-                self.underline=underline
+            if underline != self.underline:
+                self.underline = underline
                 self.f.write(escpos.ep_underline[underline])
-    def printline(self,l="",justcheckfit=False,allowwrap=True,
-                  colour=None,font=None,emph=None,underline=None):
-        self._printed=True
-        cpl=self.cpl
+    def printline(self, l="", justcheckfit=False, allowwrap=True,
+                  colour=None, font=None, emph=None, underline=None):
+        self._printed = True
+        cpl = self.cpl
         if font is not None:
-            cpl=self.fontcpl[font]
-        s=l.split("\t")
-        if len(s)>0: left=s[0]
-        else: left=""
-        if len(s)>1: center=s[1]
-        else: center=""
-        if len(s)>2: right=s[2]
-        else: right=""
-        fits=(len(left)+len(center)+len(right)<=cpl)
-        if justcheckfit: return fits
-        if not allowwrap and not fits: return False
+            cpl = self.fontcpl[font]
+        s = l.split("\t")
+        left = s[0] if len(s) > 0 else ""
+        center = s[1] if len(s) > 1 else ""
+        right = s[2] if len(s) > 2 else ""
+        fits = (len(left) + len(center) + len(right) <= cpl)
+        if justcheckfit:
+            return fits
+        if not allowwrap and not fits:
+            return False
         if colour is not None:
             self.f.write(escpos.ep_colour[colour])
         if font is not None:
@@ -450,22 +442,22 @@ class escpos(object):
         # Center is not empty, and left and right are not empty -
         # can't use any wrap.
 
-        if center=="":
-            ll=lrwrap(left,right,cpl)
+        if not center:
+            ll = lrwrap(left, right, cpl)
             for i in ll:
-                self.f.write(("%s\n"%i).encode(self.coding))
-        elif left=="" and right=="":
+                self.f.write(("%s\n" % i).encode(self.coding))
+        elif not left and not right:
             self.f.write(escpos.ep_center)
-            ll=wrap(center,cpl)
+            ll = wrap(center, cpl)
             for i in ll:
-                self.f.write(("%s\n"%i).encode(self.coding))
+                self.f.write(("%s\n" % i).encode(self.coding))
             self.f.write(escpos.ep_left)
         else:
-            pad=max(cpl-len(left)-len(center)-len(right),0)
-            padl=pad/2
-            padr=pad-padl
-            self.f.write(("%s%s%s%s%s\n"%(
-                        left,' '*padl,center,' '*padr,right)).
+            pad = max(cpl - len(left) - len(center) - len(right), 0)
+            padl = pad // 2
+            padr = pad - padl
+            self.f.write(("%s%s%s%s%s\n" % (
+                left, ' ' * padl, center, ' ' * padr, right)).
                          encode(self.coding))
         if colour is not None:
             self.f.write(escpos.ep_colour[self.colour])
@@ -476,56 +468,63 @@ class escpos(object):
         if underline is not None:
             self.f.write(escpos.ep_underline[self.underline])
         return fits
-    def checkwidth(self,line):
-        return self.printline(line,justcheckfit=True)
-    def printqrcode_native(self,data):
-        log.debug("Native QR code print: %d bytes: %s"%(len(data),repr(data)))
+    def checkwidth(self, line):
+        return self.printline(line, justcheckfit=True)
+    def printqrcode_native(self, data):
+        log.debug("Native QR code print: %d bytes: %s" % (len(data), repr(data)))
         # Set the size of a "module", in dots.  The default is apparently
         # 3 (which is also the lowest).  The maximum is 16.
-        
-        ms=16
-        if self.dpl==420: # 58mm paper width
-            if len(data)>14: ms=14
-            if len(data)>24: ms=12
-            if len(data)>34: ms=11
-            if len(data)>44: ms=10
-            if len(data)>58: ms=9
-            if len(data)>64: ms=8
-            if len(data)>84: ms=7
-            if len(data)>119: ms=6
-            if len(data)>177: ms=5
-            if len(data)>250: ms=4
-            if len(data)>439: ms=3
-            if len(data)>742: return # Too big to print
+
+        # Experience has shown that scanning QR codes from curved
+        # receipt paper with larger module sizes is difficult; it's
+        # better to keep the module size low so the camera can be
+        # closer to the paper and the paper can be held flat using
+        # fingers on the margin.
+        ms = 8
+        if self.dpl == 420: # 58mm paper width
+            #if len(data) > 14: ms = 14
+            #if len(data) > 24: ms = 12
+            #if len(data) > 34: ms = 11
+            #if len(data) > 44: ms = 10
+            #if len(data) > 58: ms = 9
+            #if len(data) > 64: ms = 8
+            if len(data) > 84: ms = 7
+            if len(data) > 119: ms = 6
+            if len(data) > 177: ms = 5
+            if len(data) > 250: ms = 4
+            if len(data) > 439: ms = 3
+            if len(data) > 742: return # Too big to print
         else: # 80mm paper width
-            if len(data)>34: ms=15
-            if len(data)>44: ms=14
-            if len(data)>45: ms=13
-            if len(data)>58: ms=12
-            if len(data)>64: ms=11
-            if len(data)>84: ms=10
-            if len(data)>119: ms=9
-            if len(data)>137: ms=8
-            if len(data)>177: ms=7
-            if len(data)>250: ms=6
-            if len(data)>338: ms=5
-            if len(data)>511: ms=4
-            if len(data)>790: ms=3
-            if len(data)>1273: return # Too big to print
-        self.f.write(ep_2d_cmd(49,67,ms))
+            #if len(data) > 34: ms = 15
+            #if len(data) > 44: ms = 14
+            #if len(data) > 45: ms = 13
+            #if len(data) > 58: ms = 12
+            #if len(data) > 64: ms = 11
+            #if len(data) > 84: ms = 10
+            #if len(data) > 119: ms = 9
+            #if len(data) > 137: ms = 8
+            if len(data) > 177: ms = 7
+            if len(data) > 250: ms = 6
+            if len(data) > 338: ms = 5
+            if len(data) > 511: ms = 4
+            if len(data) > 790: ms = 3
+            if len(data) > 1273: return # Too big to print
+        self.f.write(ep_2d_cmd(49, 67, ms))
 
         # Set error correction:
         # 48 = L = 7% recovery
         # 49 = M = 15% recovery
         # 50 = Q = 25% recovery
         # 51 = H = 30% recovery
-        self.f.write(ep_2d_cmd(49,69,51))
+        self.f.write(ep_2d_cmd(49, 69, 51))
 
         # Send QR code data
-        self.f.write(ep_2d_cmd(49,80,48,data))
+        self.f.write(ep_2d_cmd(49, 80, 48, data))
 
         # Print the QR code
-        self.f.write(ep_2d_cmd(49,81,48))
+        self.f.write(escpos.ep_center)
+        self.f.write(ep_2d_cmd(49, 81, 48))
+        self.f.write(escpos.ep_left)
     def printqrcode(self, data):
         self._printed = True
         if self.native_qrcode_support:
@@ -533,10 +532,10 @@ class escpos(object):
         if not _qrcode_supported:
             self.printline("qrcode library not installed")
             return
-        # XXX this code is known not to work at the moment!
-        q=qrcode.QRCode(border=2)
+        q = qrcode.QRCode(border=2,
+                          error_correction=qrcode.constants.ERROR_CORRECT_H)
         q.add_data(data)
-        code=q.get_matrix()
+        code = q.get_matrix()
         self.f.write(escpos.ep_unidirectional_on)
         # To get a good print, we print two rows at a time - but only
         # feed the paper through by one row.  This means that each
@@ -544,29 +543,31 @@ class escpos(object):
         # printing each pair of rows twice, advancing the paper by
         # half a dot inbetween.  We only use 6 of the 8 pins of the
         # printer to keep this code simple.
-        lt={
-            (False,False): chr(0x00),
-            (False,True): chr(0x1c),
-            (True,False): chr(0xe0),
-            (True,True): chr(0xfc),
+        lt = {
+            (False, False): bytes([0x00]),
+            (False, True): bytes([0x1c]),
+            (True, False): bytes([0xe0]),
+            (True, True): bytes([0xfc]),
             }
-        while len(code)>0:
-            if len(code)>1:
-                row=list(zip(code[0],code[1]))
+        while len(code) > 0:
+            if len(code) > 1:
+                row = zip(code[0], code[1])
             else:
-                row=list(zip(code[0],[False]*len(code[0])))
-            code=code[1:]
-            width=len(row)*3
-            if width>self.dpl: break # Code too wide for paper
-            padding=(self.dpl-width)/2
-            width=width+padding
-            padchars=chr(0)*padding
-            header=escpos.ep_bitimage_sd+chr(width&0xff)+chr((width>>8)&0xff)
-            self.f.write(header+padchars+''.join(lt[x]+lt[x]+lt[x] for x in row)
-                         +"\r".encode(self.coding))
+                row = zip(code[0], [False] * len(code[0]))
+            code = code[1:]
+            row = b''.join(lt[x] * 3 for x in row)
+            width = len(row)
+            if width > self.dpl:
+                # Code too wide for paper
+                break
+            padding = (self.dpl - width) // 2
+            width = width + padding
+            padchars = bytes([0]) * padding
+            header = escpos.ep_bitimage_sd + \
+                     bytes([width & 0xff, (width >> 8) & 0xff])
+            self.f.write(header + padchars + row + b'\r')
             self.f.write(escpos.ep_half_dot_feed)
-            self.f.write(header+padchars+''.join(lt[x]+lt[x]+lt[x] for x in row)
-                         +"\r".encode(self.coding))
+            self.f.write(header + padchars + row + b'\r')
             self.f.write(escpos.ep_short_feed)
         self.f.write(escpos.ep_unidirectional_off)
     def kickout(self):
@@ -574,28 +575,28 @@ class escpos(object):
         self.f.flush()
 
 class Epson_TM_U220_driver(escpos):
-    def __init__(self,paperwidth,coding='iso-8859-1',has_cutter=False):
+    def __init__(self, paperwidth, coding='iso-8859-1', has_cutter=False):
         # Characters per line with fonts 0 and 1
-        if paperwidth==57 or paperwidth==58:
-            cpl=(25,30)
-            dpl=148
-        elif paperwidth==76:
-            cpl=(33,40)
-            dpl=192
+        if paperwidth == 57 or paperwidth == 58:
+            cpl = (25, 30)
+            dpl = 148
+        elif paperwidth == 76:
+            cpl = (33, 40)
+            dpl = 192
         else:
             raise Exception("Unknown paper width")
-        escpos.__init__(self,cpl,dpl,coding,has_cutter,
+        escpos.__init__(self, cpl, dpl, coding, has_cutter,
                         default_font=1)
 
 class Epson_TM_T20_driver(escpos):
     def __init__(self,paperwidth,coding='iso-8859-1'):
         # Characters per line with fonts 0 and 1
-        if paperwidth==57 or paperwidth==58:
-            cpl=(35,46)
-            dpl=420
-        elif paperwidth==80:
-            cpl=(48,64)
-            dpl=576
+        if paperwidth == 57 or paperwidth == 58:
+            cpl = (35, 46)
+            dpl = 420
+        elif paperwidth == 80:
+            cpl = (48, 64)
+            dpl = 576
         else:
             raise Exception("Unknown paper width {}".format(paperwidth))
         escpos.__init__(self,cpl,dpl,coding,has_cutter=True,
