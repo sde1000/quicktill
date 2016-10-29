ce6da2a5f7969068c79d503b77b4fb769e952fce
Stephen Early <steve@assorted.org.uk>
2016-10-29 13:21:17 +0100

    PEP8 pass on delivery.py

--- quicktill/delivery.py
+++ quicktill/delivery.py
@@ -8,43 +8,43 @@ import datetime
 import logging
 log = logging.getLogger(__name__)
 
-@user.permission_required('deliveries',"List deliveries")
+@user.permission_required('deliveries', "List deliveries")
 def deliverymenu():
+    """Display a list of deliveries and call the edit function.
     """
-    Display a list of deliveries and call the edit function.
-
-    """
-    dl=td.s.query(Delivery).\
-        order_by(Delivery.checked).\
-        order_by(desc(Delivery.date)).\
-        order_by(desc(Delivery.id)).\
-        all()
-    f=ui.tableformatter(' r l l l l ')
-    lines=[(f(x.id,x.supplier.name,x.date,x.docnumber or "",
-              "" if x.checked else "not confirmed"),
-            delivery,(x.id,)) for x in dl]
-    lines.insert(0,("Record new delivery",delivery,None))
-    ui.menu(lines,title="Delivery List",
+    dl = td.s.query(Delivery)\
+             .order_by(Delivery.checked)\
+             .order_by(desc(Delivery.date))\
+             .order_by(desc(Delivery.id))\
+             .all()
+    f = ui.tableformatter(' r l l l l ')
+    lines = [(f(x.id, x.supplier.name, x.date, x.docnumber or "",
+                "" if x.checked else "not confirmed"),
+              delivery, (x.id,)) for x in dl]
+    lines.insert(0, ("Record new delivery", delivery, None))
+    ui.menu(lines, title="Delivery List",
             blurb="Select a delivery and press Cash/Enter.")
 
 class deliveryline(ui.line):
-    def __init__(self,stockitem):
+    def __init__(self, stockitem):
         ui.line.__init__(self)
-        self.stockitem=stockitem
+        self.stockitem = stockitem
         self.update()
+
     def update(self):
-        s=self.stockitem
+        s = self.stockitem
         td.s.add(s)
         try:
-            coststr="%-6.2f"%s.costprice
+            coststr = "%-6.2f" % s.costprice
         except:
-            coststr="????? "
-        self.text="%7d %-37s %-8s %s %-5.2f %-10s"%(
-            s.id,s.stocktype.format(maxw=37),s.stockunit.id,
-            coststr,s.stocktype.saleprice,ui.formatdate(s.bestbefore))
+            coststr = "????? "
+        self.text = "%7d %-37s %-8s %s %-5.2f %-10s" % (
+            s.id, s.stocktype.format(maxw=37), s.stockunit.id,
+            coststr, s.stocktype.saleprice, ui.formatdate(s.bestbefore))
 
 class delivery(ui.basicpopup):
-    """
+    """Delivery popup
+
     The delivery window allows a delivery to be edited, printed or
     confirmed.  Prior to confirmation all details of a delivery can be
     changed.  After confirmation the delivery is read-only.  The
@@ -56,140 +56,153 @@ class delivery(ui.basicpopup):
 
     If no delivery ID is passed, a new delivery will be created once a
     supplier has been chosen.
-
     """
-    def __init__(self,dn=None):
+    def __init__(self, dn=None):
         mh, mw = ui.maxwinsize()
-        if mw<80 or mh<14:
+        if mw < 80 or mh < 14:
             ui.infopopup(["Error: the screen is too small to display "
                           "the delivery dialog box.  It must be at least "
                           "80x14 characters."],
                          title="Screen width problem")
             return
         if dn:
-            d=td.s.query(Delivery).get(dn)
-        else: d=None
+            d = td.s.query(Delivery).get(dn)
+        else:
+            d = None
         if d:
-            self.dl=[deliveryline(x) for x in d.items]
-            self.dn=d.id
+            self.dl = [deliveryline(x) for x in d.items]
+            self.dn = d.id
         else:
-            self.dl=[]
-            self.dn=None
+            self.dl = []
+            self.dn = None
         if d and d.checked:
-            title="Delivery Details - %d - read only (already confirmed)"%d.id
-            cleartext="Press Clear to go back"
-            skm={keyboard.K_CASH: (self.view_line,None)}
-            readonly=True
+            title = "Delivery Details - %d - read only (already confirmed)" % d.id
+            cleartext = "Press Clear to go back"
+            skm = {keyboard.K_CASH: (self.view_line, None)}
+            readonly = True
         else:
-            title="Delivery Details"
-            if self.dn: title=title+" - %d"%self.dn
-            cleartext=None
-            skm={keyboard.K_CASH: (self.edit_line,None),
-                 keyboard.K_CANCEL: (self.deleteline,None),
-                 keyboard.K_QUANTITY: (self.duplicate_item,None)}
-            readonly=False
+            title = "Delivery Details"
+            if self.dn:
+                title = title + " - %d" % self.dn
+            cleartext = None
+            skm = {keyboard.K_CASH: (self.edit_line, None),
+                   keyboard.K_CANCEL: (self.deleteline, None),
+                   keyboard.K_QUANTITY: (self.duplicate_item, None)}
+            readonly = False
         # The window can be as tall as the screen; we expand the scrollable
         # field to fit.  The scrollable field must be at least three lines
         # high!
-        ui.basicpopup.__init__(self,mh-1,80,title=title,cleartext=cleartext,
-                               colour=ui.colour_input)
-        self.addstr(2,2,"       Supplier:")
-        self.addstr(3,2,"           Date:")
-        self.addstr(4,2,"Document number:")
-        self.addstr(6,1,"StockNo Stock Type........................... "
-                        "Unit.... Cost.. Sale  BestBefore")
+        ui.basicpopup.__init__(
+            self, mh - 1, 80, title=title, cleartext=cleartext,
+            colour=ui.colour_input)
+        self.addstr(2, 2, "       Supplier:")
+        self.addstr(3, 2, "           Date:")
+        self.addstr(4, 2, "Document number:")
+        self.addstr(6, 1, "StockNo Stock Type........................... "
+                    "Unit.... Cost.. Sale  BestBefore")
         self.supfield = ui.modelfield(
             2, 19, 59, Supplier, 'name', default=d.supplier if d else None,
             create=createsupplier, readonly=readonly)
         # If there is not yet an underlying Delivery object, the window
         # can be dismissed by pressing Clear on the supplier field
         if self.dn is None:
-            self.supfield.keymap[keyboard.K_CLEAR]=(self.dismiss,None)
-        self.datefield=ui.datefield(
-            3,19,f=d.date if d else datetime.date.today(),
+            self.supfield.keymap[keyboard.K_CLEAR] = (self.dismiss, None)
+        self.datefield = ui.datefield(
+            3, 19, f=d.date if d else datetime.date.today(),
             readonly=readonly)
-        self.docnumfield=ui.editfield(4,19,40,f=d.docnumber if d else "",
-                                      readonly=readonly)
-        self.entryprompt=None if readonly else ui.line(
+        self.docnumfield = ui.editfield(4, 19, 40, f=d.docnumber if d else "",
+                                        readonly=readonly)
+        self.entryprompt = None if readonly else ui.line(
             " [ New item ]")
-        self.s=ui.scrollable(
-            7,1,78,mh-9 if readonly else mh-11,self.dl,
-            lastline=self.entryprompt,keymap=skm)
+        self.s = ui.scrollable(
+            7, 1, 78, mh - 9 if readonly else mh - 11, self.dl,
+            lastline=self.entryprompt, keymap=skm)
         if readonly:
             self.s.focus()
         else:
-            self.deletefield=ui.buttonfield(
-                mh-3,2,24,"Delete this delivery",keymap={
-                    keyboard.K_CASH: (self.confirmdelete,None)})
-            self.confirmfield=ui.buttonfield(
-                mh-3,28,31,"Confirm details are correct",keymap={
-                    keyboard.K_CASH: (self.confirmcheck,None)})
-            self.savefield=ui.buttonfield(
-                mh-3,61,17,"Save and exit",keymap={
-                    keyboard.K_CASH: (self.finish,None)})
+            self.deletefield = ui.buttonfield(
+                mh - 3, 2, 24, "Delete this delivery", keymap={
+                    keyboard.K_CASH: (self.confirmdelete, None)})
+            self.confirmfield = ui.buttonfield(
+                mh - 3, 28, 31, "Confirm details are correct", keymap={
+                    keyboard.K_CASH: (self.confirmcheck, None)})
+            self.savefield = ui.buttonfield(
+                mh - 3, 61, 17, "Save and exit", keymap={
+                    keyboard.K_CASH: (self.finish, None)})
             ui.map_fieldlist(
-                [self.supfield,self.datefield,self.docnumfield,self.s,
-                 self.deletefield,self.confirmfield,self.savefield])
-            self.supfield.sethook=self.update_model
-            self.datefield.sethook=self.update_model
-            self.docnumfield.sethook=self.update_model
+                [self.supfield, self.datefield, self.docnumfield, self.s,
+                 self.deletefield, self.confirmfield, self.savefield])
+            self.supfield.sethook = self.update_model
+            self.datefield.sethook = self.update_model
+            self.docnumfield.sethook = self.update_model
             if self.dn:
                 self.s.focus()
             else:
                 self.supfield.focus()
+
     def update_model(self):
         # Called whenever one of the three fields at the top changes.
         # If the three fields are valid and we have a Delivery model,
         # update it.  If any of them are not valid, or there is not
         # yet a Delivery model, do nothing.
-        if self.supfield.f is None: return
-        date=self.datefield.read()
-        if not date: return
-        if self.docnumfield.f=="": return
-        if not self.dn: return
-        d=td.s.query(Delivery).get(self.dn)
-        d.supplier=self.supfield.read()
-        d.date=date
-        d.docnumber=self.docnumfield.f
+        if self.supfield.f is None:
+            return
+        date = self.datefield.read()
+        if not date:
+            return
+        if self.docnumfield.f == "":
+            return
+        if not self.dn:
+            return
+        d = td.s.query(Delivery).get(self.dn)
+        d.supplier = self.supfield.read()
+        d.date = date
+        d.docnumber = self.docnumfield.f
         td.s.flush()
+
     def make_delivery_model(self):
         # If we do not have a delivery ID, create one if possible.  If
         # we still don't have one after this, it's because a required
         # field was missing and we've just popped up an error message
         # about it.
-        if self.dn: return
-        if self.supfield.f is None:
-            ui.infopopup(["Select a supplier before continuing!"],title="Error")
+        if self.dn:
             return
-        date=self.datefield.read()
+        if self.supfield.f is None:
+            ui.infopopup(["Select a supplier before continuing!"], title="Error")
+            return
+        date = self.datefield.read()
         if date is None:
             ui.infopopup(["Check that the delivery date is correct before "
-                          "continuing!"],title="Error")
+                          "continuing!"], title="Error")
             return
-        if self.docnumfield.f=="":
+        if self.docnumfield.f == "":
             ui.infopopup(["Enter a document number before continuing!"],
                          title="Error")
             return
-        d=Delivery()
-        d.supplier=self.supfield.read()
-        d.date=date
-        d.docnumber=self.docnumfield.f
+        d = Delivery()
+        d.supplier = self.supfield.read()
+        d.date = date
+        d.docnumber = self.docnumfield.f
         td.s.add(d)
         td.s.flush()
-        self.dn=d.id
+        self.dn = d.id
         del self.supfield.keymap[keyboard.K_CLEAR]
+        self.addstr(0, 1, "Delivery Details - {}".format(d.id))
+
     def finish(self):
         # Save and exit button
         self.make_delivery_model()
         if self.dn:
             self.dismiss()
+
     def reallydeleteline(self):
-        item=self.dl[self.s.cursor].stockitem
+        item = self.dl[self.s.cursor].stockitem
         td.s.add(item)
         td.s.delete(item)
         del self.dl[self.s.cursor]
         td.s.flush()
         self.s.drawdl()
+
     def deleteline(self):
         if not self.s.cursor_on_lastline():
             td.s.add(self.dl[self.s.cursor].stockitem)
@@ -197,10 +210,11 @@ class delivery(ui.basicpopup):
                 ["Press Cash/Enter to confirm deletion of stock "
                  "number %d.  Note that once it's deleted you can't "
                  "create a new stock item with the same number; new "
-                 "stock items always get fresh numbers."%(
+                 "stock items always get fresh numbers." % (
                         self.dl[self.s.cursor].stockitem.id)],
                 title="Confirm Delete",
-                keymap={keyboard.K_CASH:(self.reallydeleteline,None,True)})
+                keymap={keyboard.K_CASH: (self.reallydeleteline, None, True)})
+
     def printout(self):
         if self.dn is None:
             return
@@ -209,16 +223,19 @@ class delivery(ui.basicpopup):
                 for x in printer.labelprinters]
         ui.automenu(menu, title="Delivery print options",
                     colour=ui.colour_confirm)
+
     def reallyconfirm(self):
-        if not self.dn: return
-        d=td.s.query(Delivery).get(self.dn)
-        d.checked=True
+        if not self.dn:
+            return
+        d = td.s.query(Delivery).get(self.dn)
+        d.checked = True
         td.s.flush()
         self.dismiss()
         usestock.auto_allocate_internal(deliveryid=self.dn,
                                         message_on_no_work=False)
+
     def confirmcheck(self):
-        if not self.dn:
+        if not self.dn or not self.dl:
             ui.infopopup(["There is nothing here to confirm!"],
                          title="Error")
             return
@@ -227,61 +244,71 @@ class delivery(ui.basicpopup):
                       "of the delivery.  Once the delivery is confirmed, you "
                       "can't go back and change any of the details.  Press "
                       "Cash/Enter to confirm this delivery now, or Clear to "
-                      "continue editing it."],title="Confirm Details",
-                     keymap={keyboard.K_CASH:(self.reallyconfirm,None,True)})
-    def line_edited(self,stockitem):
+                      "continue editing it."], title="Confirm Details",
+                     keymap={keyboard.K_CASH: (self.reallyconfirm, None, True)})
+
+    def line_edited(self, stockitem):
         # Only called when a line has been edited; not called for new
         # lines or deletions
         self.dl[self.s.cursor].update()
         self.s.cursor_down()
-    def newline(self,stockitem):
+
+    def newline(self, stockitem):
         self.dl.append(deliveryline(stockitem))
         self.s.cursor_down()
+
     def edit_line(self):
         # If there is not yet an underlying Delivery object, create one
         self.make_delivery_model()
-        if self.dn is None: return # with errors already popped up
+        if self.dn is None:
+            return # with errors already popped up
         # If it's the "lastline" then we create a new stock item
         if self.s.cursor_on_lastline():
-            stockitem(self.newline,self.dn)
+            stockitem(self.newline, self.dn)
         else:
             td.s.add(self.dl[self.s.cursor].stockitem)
-            stockitem(self.line_edited,self.dn,self.dl[self.s.cursor].stockitem)
+            stockitem(self.line_edited, self.dn, self.dl[self.s.cursor].stockitem)
+
     def view_line(self):
         # In read-only mode there is no "lastline"
         td.s.add(self.dl[self.s.cursor].stockitem)
         stock.stockinfo_popup(self.dl[self.s.cursor].stockitem.id)
+
     def duplicate_item(self):
-        existing=self.dl[len(self.dl)-1 if self.s.cursor_on_lastline()
-                         else self.s.cursor].stockitem
+        existing = self.dl[len(self.dl) - 1 if self.s.cursor_on_lastline()
+                           else self.s.cursor].stockitem
         td.s.add(existing)
         # We deliberately do not copy the best-before date, because it
         # might be different on the new item.
-        new=StockItem(delivery=existing.delivery,stocktype=existing.stocktype,
-                      stockunit=existing.stockunit,costprice=existing.costprice)
+        new = StockItem(
+            delivery=existing.delivery, stocktype=existing.stocktype,
+            stockunit=existing.stockunit, costprice=existing.costprice)
         td.s.add(new)
         td.s.flush()
         self.dl.append(deliveryline(new))
         self.s.cursor_down()
+
     def reallydelete(self):
         if self.dn is None:
             self.dismiss()
             return
-        d=td.s.query(Delivery).get(self.dn)
+        d = td.s.query(Delivery).get(self.dn)
         for i in d.items:
             td.s.delete(i)
         td.s.delete(d)
         self.dismiss()
+
     def confirmdelete(self):
         ui.infopopup(["Do you want to delete the entire delivery and all "
                       "the stock items that have been entered for it?  "
                       "Press Cancel to delete or Clear to go back."],
                      title="Confirm Delete",
-                     keymap={keyboard.K_CANCEL:(self.reallydelete,None,True)})
-    def keypress(self,k):
-        if k==keyboard.K_PRINT:
+                     keymap={keyboard.K_CANCEL: (self.reallydelete, None, True)})
+
+    def keypress(self, k):
+        if k == keyboard.K_PRINT:
             self.printout()
-        elif k==keyboard.K_CLEAR:
+        elif k == keyboard.K_CLEAR:
             self.dismiss()
 
 class stockitem(ui.basicpopup):
@@ -296,7 +323,7 @@ class stockitem(ui.basicpopup):
         self.func = func
         self.item = item
         self.deliveryid = deliveryid
-        cleartext=(
+        cleartext = (
             "Press Clear to exit, forgetting all changes" if item else
             "Press Clear to exit without creating a new stock item")
         ui.basicpopup.__init__(self, 13, 78, title="Stock Item",
@@ -472,7 +499,7 @@ def createsupplier(field, name):
     editsupplier(lambda supplier: field.set(supplier), defaultname=name)
 
 class editsupplier(ui.basicpopup):
-    @user.permission_required('edit-supplier',"Create or edit supplier details")
+    @user.permission_required('edit-supplier', "Create or edit supplier details")
     def __init__(self, func, supplier=None, defaultname=None):
         if supplier:
             td.s.add(supplier)
@@ -503,6 +530,7 @@ class editsupplier(ui.basicpopup):
         ui.map_fieldlist([self.namefield, self.telfield, self.emailfield,
                           self.webfield])
         self.namefield.focus()
+
     def confirmwin(self):
         # Called when Cash/Enter is pressed on the last field, for new
         # suppliers only
@@ -515,6 +543,7 @@ class editsupplier(ui.basicpopup):
                      title="Confirm New Supplier Details",
                      colour=ui.colour_input,keymap={
             keyboard.K_CASH: (self.confirmed, None, True)})
+
     def confirmed(self):
         if self.sn:
             supplier = td.s.query(Supplier).get(self.sn)
@@ -535,6 +564,6 @@ class editsupplier(ui.basicpopup):
 def updatesupplier():
     log.info("Update supplier")
     sl = td.s.query(Supplier).order_by(Supplier.name).all()
-    m=[(x.name, editsupplier, (lambda a:None, x)) for x in sl]
+    m = [(x.name, editsupplier, (lambda a:None, x)) for x in sl]
     ui.menu(m, blurb="Select a supplier from the list and press Cash/Enter.",
             title="Edit Supplier")
