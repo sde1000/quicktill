a7a698ba5eeccf2fdae85d643864b1ecbc1d41c0
Stephen Early <steve@assorted.org.uk>
2016-11-13 11:36:56 +0000

    Fix the twitter client
    The twitter client is now far from full-featured; it might be a good
    idea to remove it at some point.  Most tweets seem to contain links to
    media that can't be displayed on the till.
    
    Closes #117 on github.

--- quicktill/extras.py
+++ quicktill/extras.py
@@ -167,12 +167,11 @@ class reminderpopup(object):
 
 class twitter_auth(cmdline.command):
     """Generate tokens for Twitter login.
-
     """
     @staticmethod
     def add_arguments(subparsers):
-        parser=subparsers.add_parser(
-            'twitter-auth',help="authorise with Twitter",
+        parser = subparsers.add_parser(
+            'twitter-auth', help="authorise with Twitter",
             description=twitter_auth.__doc__)
         parser.add_argument("--consumer-key", action="store",
                             dest="consumer_key",
@@ -181,30 +180,33 @@ class twitter_auth(cmdline.command):
                             dest="consumer_secret",
                             help="OAuth1 consumer secret")
         parser.set_defaults(command=twitter_auth.run)
+
     @staticmethod
     def run(args):
         if not args.consumer_key:
             args.consumer_key = input("Consumer key: ")
         if not args.consumer_secret:
             args.consumer_secret = input("Consumer secret: ")
-        request_token_url='https://api.twitter.com/oauth/request_token'
+        request_token_url = 'https://api.twitter.com/oauth/request_token'
         base_authorize_url = 'https://api.twitter.com/oauth/authorize'
         access_token_url = 'https://api.twitter.com/oauth/access_token'
-        oauth=OAuth1Session(args.consumer_key,client_secret=args.consumer_secret)
-        fetch_response=oauth.fetch_request_token(request_token_url)
-        resource_owner_key=fetch_response.get('oauth_token')
-        resource_owner_secret=fetch_response.get('oauth_token_secret')
-        authorize_url=oauth.authorization_url(base_authorize_url)
+        oauth = OAuth1Session(
+            args.consumer_key, client_secret=args.consumer_secret)
+        fetch_response = oauth.fetch_request_token(request_token_url)
+        resource_owner_key = fetch_response.get('oauth_token')
+        resource_owner_secret = fetch_response.get('oauth_token_secret')
+        authorize_url = oauth.authorization_url(base_authorize_url)
         print("Please visit this URL: {}".format(authorize_url))
         verifier = input("Enter the PIN from Twitter: ")
-        oauth=OAuth1Session(args.consumer_key,
-                            client_secret=args.consumer_secret,
-                            resource_owner_key=resource_owner_key,
-                            resource_owner_secret=resource_owner_secret,
-                            verifier=verifier)
-        oauth_tokens=oauth.fetch_access_token(access_token_url)
-        resource_owner_key=oauth_tokens.get('oauth_token')
-        resource_owner_secret=oauth_tokens.get('oauth_token_secret')
+        oauth = OAuth1Session(
+            args.consumer_key,
+            client_secret=args.consumer_secret,
+            resource_owner_key=resource_owner_key,
+            resource_owner_secret=resource_owner_secret,
+            verifier=verifier)
+        oauth_tokens = oauth.fetch_access_token(access_token_url)
+        resource_owner_key = oauth_tokens.get('oauth_token')
+        resource_owner_secret = oauth_tokens.get('oauth_token_secret')
 
         tapi = twython.Twython(
             app_key=args.consumer_key,
@@ -216,15 +218,15 @@ class twitter_auth(cmdline.command):
         print("Paste the following to enable Twitter access as @{}:".format(
             user['screen_name']))
         print("""
-        tapi=extras.twitter_api(
+        tapi = quicktill.extras.twitter_api(
             token='{}',
             token_secret='{}',
             consumer_key='{}',
             consumer_secret='{}')""".format(
-                resource_owner_key,resource_owner_secret,
-                args.consumer_key,args.consumer_secret))
+                resource_owner_key, resource_owner_secret,
+                args.consumer_key, args.consumer_secret))
 
-def twitter_api(token,token_secret,consumer_key,consumer_secret):
+def twitter_api(token, token_secret, consumer_key, consumer_secret):
     return twython.Twython(
         app_key=consumer_key,
         app_secret=consumer_secret,
@@ -232,101 +234,106 @@ def twitter_api(token,token_secret,consumer_key,consumer_secret):
         oauth_token_secret=token_secret)
 
 class twitter_post(ui.dismisspopup):
-    def __init__(self,tapi,default_text="",fail_silently=False):
+    def __init__(self, tapi, default_text="", fail_silently=False):
         try:
-            user=tapi.verify_credentials()
+            user = tapi.verify_credentials()
         except:
             if not fail_silently:
                 ui.infopopup(["Unable to connect to Twitter"],
                              title="Error")
             return
-        ui.dismisspopup.__init__(self,7,76,
-                                 title="@%s Twitter"%user["screen_name"],
+        ui.dismisspopup.__init__(self, 7, 76,
+                                 title="@{} Twitter".format(user["screen_name"]),
                                  dismiss=keyboard.K_CLEAR,
                                  colour=ui.colour_input)
-        self.tapi=tapi
-        self.addstr(2,2,"Type in your update here and press Enter:")
-        self.tfield=ui.editfield(
-            4,2,72,f=default_text,flen=140,keymap={
-                keyboard.K_CLEAR: (self.dismiss,None),
-                keyboard.K_CASH: (self.enter,None,False)})
+        self.tapi = tapi
+        self.addstr(2, 2, "Type in your update here and press Enter:")
+        self.tfield = ui.editfield(
+            4, 2, 72, f=default_text, flen=140, keymap={
+                keyboard.K_CLEAR: (self.dismiss, None),
+                keyboard.K_CASH: (self.enter, None, False)})
         self.tfield.focus()
+
     def enter(self):
-        ttext=self.tfield.f
-        if len(ttext)<20:
-            ui.infopopup(title="Twitter Problem",text=[
+        ttext = self.tfield.f
+        if len(ttext) < 20:
+            ui.infopopup(title="Twitter Problem", text=[
                     "That's too short!  Try typing some more."])
             return
         try:
             self.tapi.update_status(status=ttext)
             self.dismiss()
-            ui.infopopup(title="Tweeted",text=["Your update has been posted."],
-                         dismiss=keyboard.K_CASH,colour=ui.colour_confirm)
+            ui.infopopup(title="Tweeted", text=["Your update has been posted."],
+                         dismiss=keyboard.K_CASH, colour=ui.colour_confirm)
         except:
             ui.popup_exception("Error posting tweet")
 
 class Tweet(ui.lrline):
-    def __init__(self,status):
-        self.status=status
-        ui.lrline.__init__(self,ltext=status.text,
-                           rtext="(@%s %s)"%(
-                status.user.screen_name,status.relative_created_at))
+    def __init__(self, status):
+        self.status = status
+        ui.lrline.__init__(
+            self, ltext=status["text"],
+            rtext="(@{} {})".format(
+                status["user"]["screen_name"], status["created_at"]))
 
-class twitter_client(user.permission_checked,ui.dismisspopup):
-    permission_required=("twitter","Use the Twitter client")
-    def __init__(self,tapi):
-        (mh,mw)=ui.maxwinsize()
+class twitter_client(user.permission_checked, ui.dismisspopup):
+    permission_required = ("twitter", "Use the Twitter client")
+    def __init__(self, tapi):
+        mh, mw = ui.maxwinsize()
         # We want to make our window very-nearly full screen
-        w=mw-4
-        h=mh-2
-        self.tapi=tapi
+        w = mw - 4
+        h = mh - 2
+        self.tapi = tapi
         try:
-            user=tapi.verify_credentials()
+            user = tapi.verify_credentials()
         except:
             ui.infopopup(["Unable to connect to Twitter"],
                          title="Error")
             return
-        ui.dismisspopup.__init__(self,h,w,title="@%s Twitter"%user["screen_name"],
-                                 dismiss=keyboard.K_CLEAR,
-                                 colour=ui.colour_input)
-        self.tl=[]
+        ui.dismisspopup.__init__(
+            self, h, w, title="@{} Twitter".format(user["screen_name"]),
+            dismiss=keyboard.K_CLEAR,
+            colour=ui.colour_input)
+        self.tl = []
         # The first line of the window is a text entry line for tweeting.
         # The rest of the window is a scrollable list of tweets by us and
         # by other people.
-        self.addstr(2,2,"Your tweet: ")
-        self.tfield=ui.editfield(
-            2,14,w-16,flen=140,keymap={
-                keyboard.K_CLEAR: (self.dismiss,None),
-                keyboard.K_CASH: (self.enter,None,False)})
-        self.tweets=ui.scrollable(4,2,w-4,h-6,self.tl,keymap={
-                keyboard.K_CASH: (self.reply,None,False)})
-        self.rbutton=ui.buttonfield(
-            h-2,2,18,"Refresh",keymap={
-                keyboard.K_CASH: (self.refresh,None)})
-        ui.map_fieldlist([self.tfield,self.tweets,self.rbutton])
+        self.addstr(2, 2, "Your tweet: ")
+        self.tfield = ui.editfield(
+            2, 14, w - 16, flen=140, keymap={
+                keyboard.K_CLEAR: (self.dismiss, None),
+                keyboard.K_CASH: (self.enter, None, False)})
+        self.tweets = ui.scrollable(4, 2, w - 4, h - 6, self.tl, keymap={
+                keyboard.K_CASH: (self.reply, None, False)})
+        self.rbutton = ui.buttonfield(
+            h - 2, 2, 18, "Refresh", keymap={
+                keyboard.K_CASH: (self.refresh, None)})
+        ui.map_fieldlist([self.tfield, self.tweets, self.rbutton])
         self.refresh()
+
     def enter(self):
-        ttext=self.tfield.f
-        if len(ttext)<20:
-            ui.infopopup(title="Twitter Problem",text=[
-                    "That's too short!  Try typing some more."])
+        ttext = self.tfield.f
+        if len(ttext) < 20:
+            ui.infopopup(title="Twitter Problem", text=[
+                "That's too short!  Try typing some more."])
             return
-        status=self.tapi.update_status(status=ttext)
+        status = self.tapi.update_status(status=ttext)
         self.tfield.set("")
-        self.timeline.insert(0,status)
-        self.tl.insert(0,Tweet(status))
+        self.timeline.insert(0, status)
+        self.tl.insert(0, Tweet(status))
         self.tweets.redraw()
         self.tfield.focus()
+
     def reply(self):
         # Fill field with reply info
-        self.tfield.set("@%s: "%self.timeline[self.tweets.cursor].
-                        user["screen_name"])
+        self.tfield.set("@{}: ".format(
+            self.timeline[self.tweets.cursor]["user"]["screen_name"]))
         self.tfield.focus()
+
     def refresh(self):
-        self.timeline=self.tapi.get_home_timeline(count=20)
-        #self.timeline=self.timeline+self.tapi.GetReplies()
-        self.timeline.sort(key=lambda x:x.created_at_in_seconds)
+        self.timeline = self.tapi.get_home_timeline(count=20)
+        self.timeline.sort(key=lambda x: x["id_str"])
         self.timeline.reverse()
-        self.tl=[Tweet(x) for x in self.timeline]
+        self.tl = [Tweet(x) for x in self.timeline]
         self.tweets.set(self.tl)
         self.tfield.focus()
