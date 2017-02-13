011d08d883f2030e5d17985864a08b3d8dcb8270
Stephen Early <steve@assorted.org.uk>
2017-02-13 12:14:59 +0000

    Add python_2_unicode_compatible decorator to tillweb models

--- quicktill/tillweb/models.py
+++ quicktill/tillweb/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -12,6 +13,7 @@ def get_app_details(user):
                  'objects':tills}]
     return []
 
+@python_2_unicode_compatible
 class Till(models.Model):
     """
     A till database that we want to refer to.  NB the database
@@ -25,7 +27,10 @@ class Till(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('quicktill.tillweb.views.pubroot',[self.slug])
-    def __unicode__(self): return self.name
+
+    def __str__(self):
+        return self.name
+
     def nav(self):
         return [self]
     def navtext(self):
@@ -37,6 +42,7 @@ PERMISSIONS=(
     ('F','Full access'),
 )
 
+@python_2_unicode_compatible
 class Access(models.Model):
     """
     Access to a till database by a particular user.  Also encodes the
@@ -49,6 +55,7 @@ class Access(models.Model):
     permission=models.CharField(max_length=1,choices=PERMISSIONS)
     class Meta(object):
         unique_together = ( ("till", "user"), )
-    def __unicode__(self):
+
+    def __str__(self):
         return "%s can access %s %s"%(
             self.user.username,self.till,self.get_permission_display())
