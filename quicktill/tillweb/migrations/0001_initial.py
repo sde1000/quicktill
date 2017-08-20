40d1875e35d8f3a6c8e2f3d89b6c16d9a55b734a
Stephen Early <steve@assorted.org.uk>
2017-08-20 14:30:52 +0100

    Add migrations support to tillweb
    Existing tillweb installations will need to run
    
    till@till:~/tillweb$ ./manage.py migrate --fake-initial

---
+++ quicktill/tillweb/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Access',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('permission', models.CharField(max_length=1, choices=[('R', 'Read-only'), ('M', 'Pub manager'), ('F', 'Full access')])),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Till',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('slug', models.SlugField()),
+                ('name', models.TextField()),
+                ('database', models.TextField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='access',
+            name='till',
+            field=models.ForeignKey(to='tillweb.Till'),
+        ),
+        migrations.AddField(
+            model_name='access',
+            name='user',
+            field=models.ForeignKey(related_name='till_access', to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='access',
+            unique_together=set([('till', 'user')]),
+        ),
+    ]
