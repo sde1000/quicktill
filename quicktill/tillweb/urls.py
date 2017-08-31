8dd2467e78b8b98e13136f9b93bfd1e0ce5eb946
Stephen Early <steve@assorted.org.uk>
2017-08-31 18:41:58 +0100

    Use callables for views in tillweb urlconf
    Using patterns() and strings instead of callables as views will cease to
    work in django-1.10

--- quicktill/tillweb/urls.py
+++ quicktill/tillweb/urls.py
@@ -1,53 +1,44 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from quicktill.tillweb.views import *
 
-tillurls = patterns(
-    'quicktill.tillweb.views',
+tillurls = [
+    url(r'^$', pubroot),
+    url(r'^session/$', sessionfinder),
+    url(r'^session/(?P<sessionid>\d+)/', include([
+        url(r'^$', session),
+        url(r'^takings-by-dept.html$', session_takings_by_dept),
+        url(r'^takings-by-user.html$', session_takings_by_user),
+        url(r'^stock-sold.html$', session_stock_sold),
+        url(r'^transactions.html$', session_transactions),
+        url(r'^sales-pie-chart.svg$', session_sales_pie_chart),
+        url(r'^users-pie-chart.svg$', session_users_pie_chart),
+        url(r'^dept(?P<dept>\d+)/$', sessiondept, name="sessiondept"),
+        ])),
+    url(r'^transaction/(?P<transid>\d+)/$', transaction),
+    url(r'^transline/(?P<translineid>\d+)/$', transline),
+    url(r'^supplier/$', supplierlist),
+    url(r'^supplier/(?P<supplierid>\d+)/$', supplier),
+    url(r'^delivery/$', deliverylist),
+    url(r'^delivery/(?P<deliveryid>\d+)/$', delivery),
+    url(r'^stocktype/$', stocktypesearch),
+    url(r'^stocktype/(?P<stocktype_id>\d+)/$', stocktype),
+    url(r'^stock/$', stocksearch),
+    url(r'^stock/(?P<stockid>\d+)/$', stock),
+    url(r'^stockline/$', stocklinelist),
+    url(r'^stockline/(?P<stocklineid>\d+)/$', stockline),
+    url(r'^plu/$', plulist),
+    url(r'^plu/(?P<pluid>\d+)/$', plu),
+    url(r'^location/$', locationlist),
+    url(r'^location/(?P<location>[\w\- ]+)/$', location),
+    url(r'^department/$', departmentlist),
+    url(r'^department/(?P<departmentid>\d+)/$', department),
+    url(r'^stockcheck/$', stockcheck),
+    url(r'^user/$', userlist),
+    url(r'^user/(?P<userid>\d+)/$', user),
+]
 
-    url(r'^$', 'pubroot'),
-    url(r'^session/$', 'sessionfinder'),
-
-    url(r'^session/(?P<sessionid>\d+)/$', 'session'),
-    url(r'^session/(?P<sessionid>\d+)/takings-by-dept.html$',
-        'session_takings_by_dept'),
-    url(r'^session/(?P<sessionid>\d+)/takings-by-user.html$',
-        'session_takings_by_user'),
-    url(r'^session/(?P<sessionid>\d+)/stock-sold.html$',
-        'session_stock_sold'),
-    url(r'^session/(?P<sessionid>\d+)/transactions.html$',
-        'session_transactions'),
-    url(r'^session/(?P<sessionid>\d+)/sales-pie-chart.svg$',
-        'session_sales_pie_chart'),
-    url(r'^session/(?P<sessionid>\d+)/users-pie-chart.svg$',
-        'session_users_pie_chart'),
-    url(r'^session/(?P<sessionid>\d+)/dept(?P<dept>\d+)/$',
-        'sessiondept', name="sessiondept"),
-
-    url(r'^transaction/(?P<transid>\d+)/$', 'transaction'),
-    url(r'^transline/(?P<translineid>\d+)/$', 'transline'),
-    url(r'^supplier/$', 'supplierlist'),
-    url(r'^supplier/(?P<supplierid>\d+)/$', 'supplier'),
-    url(r'^delivery/$', 'deliverylist'),
-    url(r'^delivery/(?P<deliveryid>\d+)/$', 'delivery'),
-    url(r'^stocktype/$', 'stocktypesearch'),
-    url(r'^stocktype/(?P<stocktype_id>\d+)/$', 'stocktype'),
-    url(r'^stock/$', 'stocksearch'),
-    url(r'^stock/(?P<stockid>\d+)/$', 'stock'),
-    url(r'^stockline/$', 'stocklinelist'),
-    url(r'^stockline/(?P<stocklineid>\d+)/$', 'stockline'),
-    url(r'^plu/$', 'plulist'),
-    url(r'^plu/(?P<pluid>\d+)/$', 'plu'),
-    url(r'^location/$', 'locationlist'),
-    url(r'^location/(?P<location>[\w\- ]+)/$', 'location'),
-    url(r'^department/$', 'departmentlist'),
-    url(r'^department/(?P<departmentid>\d+)/$', 'department'),
-    url(r'^stockcheck/$', 'stockcheck'),
-    url(r'^user/$', 'userlist'),
-    url(r'^user/(?P<userid>\d+)/$', 'user'),
-)
-
-urls = patterns(
-    'quicktill.tillweb.views',
+urls = [
     # Index page
-    url(r'^$', 'publist', name="publist"),
+    url(r'^$', publist, name="publist"),
     url(r'^(?P<pubname>[\w\-]+)/', include(tillurls)),
-)
+]
