36c42007139befa5c14b6742aa46b4c78e3b4064
Stephen Early <steve@assorted.org.uk>
2018-03-13 20:21:40 +0000

    Use proportional font in lockscreen if available

--- quicktill/lockscreen.py
+++ quicktill/lockscreen.py
@@ -7,12 +7,13 @@ log = logging.getLogger(__name__)
 
 class lockpage(ui.basicpage):
     def __init__(self):
-        ui.basicpage.__init__(self)
+        super().__init__()
         self.win.set_cursor(False)
         self.idle_timeout = None
-        self.win.addstr(1, 1, "This till is locked.")
         self.updateheader()
-        self._y = 3
+        self._y = 1
+        self.line("This till is locked.")
+        self._y += 1
         unsaved = [p for p in ui.basicpage._pagelist if p != self]
         if unsaved:
             self.line("The following users have unsaved work "
@@ -37,15 +38,14 @@ class lockpage(ui.basicpage):
         if kpproblem:
             self.line("Kitchen printer problem: {}".format(kpproblem))
             log.info("Kitchen printer problem: %s",kpproblem)
-        self.win.addstr(
-            self.h - 1, 0, "Till version: {}".format(version.version))
+        self.win.wrapstr(
+            self.h - 1, 0, self.w, "Till version: {}".format(version.version))
         self.win.move(0, 0)
         log.info("lockpage gc stats: %s, len(gc.garbage)=%d", gc.get_count(),
                  len(gc.garbage))
 
     def line(self, s):
-        self.win.addstr(self._y, 1, s)
-        self._y = self._y + 1
+        self._y += self.win.wrapstr(self._y, 1, self.w, s)
 
     def pagename(self):
         return "Lock"
@@ -58,7 +58,7 @@ class lockpage(ui.basicpage):
 
     def deselect(self):
         # This page ceases to exist when it disappears.
-        ui.basicpage.deselect(self)
+        super().deselect()
         self.dismiss()
         if self.idle_timeout:
             self.idle_timeout.cancel()
