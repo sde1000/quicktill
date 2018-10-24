75310561b668ca4ef1fc360ed3af8ce5b33fe8a5
Stephen Early <steve@assorted.org.uk>
2018-10-24 16:17:59 +0100

    Add option to allow card payment references to be blank
    Adding ref_required=False to card.CardPayment() allows users to enter a
    blank reference for card payments.
    
    Fixes #171 on github.
    
    Also removes the cashback_first option that nobody is still using, and
    considerably simplifies the popup layout code!

--- quicktill/card.py
+++ quicktill/card.py
@@ -6,90 +6,78 @@ from decimal import Decimal
 import datetime
 
 
-class cardpopup(ui.dismisspopup):
+class _cardpopup(ui.dismisspopup):
     """Ask for card payment details
 
     A window that pops up whenever a card payment is accepted.  It
     prompts for the card receipt number (provided by the credit card
     terminal) and whether there is any cashback on this transaction.
     """
-    def __init__(self, reg, transid, amount, func, max_cashback, cashback_first,
-                 ask_for_machine_id=False, refund=False):
+    def __init__(self, pm, reg, transid, amount, max_cashback, refund=False):
+        self.pm = pm
         self.reg = reg
         self.transid = transid
         self.amount = amount
-        self.func = func
         self.max_cashback = max_cashback
-        self.ask_for_machine_id = ask_for_machine_id
         cashback_in_use = max_cashback > zero
         h = 18 if cashback_in_use else 10
-        if ask_for_machine_id:
+        if self.pm._ask_for_machine_id:
             h += 1
         desc = "refund" if refund else "payment"
-        ui.dismisspopup.__init__(
-            self, h, 44, title="Card {} transaction {}".format(
+        super().__init__(
+            h, 44, title="Card {} transaction {}".format(
                 desc, transid), colour=ui.colour_input)
-        self.addstr(2, 2, "Card {} of {}".format(desc, tillconfig.fc(amount)))
+        self.win.wrapstr(
+            2, 2, 40, "Card {} of {}".format(desc, tillconfig.fc(amount)))
+        y = 4
+        fields = []
         if cashback_in_use:
-            if cashback_first:
-                cbstart = 4
-                rnstart = 12
-            else:
-                cbstart = 10 if ask_for_machine_id else 9
-                rnstart = 4
-        else:
-            rnstart = 4
-        self.addstr(rnstart, 2, "Please enter the receipt number from the")
-        self.addstr(rnstart + 1, 2, "credit card receipt.")
+            y += self.win.wrapstr(
+                y, 2, 40,
+                "Is there any cashback?  Enter the amount and "
+                "press Cash/Enter.  Leave blank and press "
+                "Cash/Enter if there is none.")
+            y += 1
+            self.win.addstr(y, 2, "Cashback amount: %s" % tillconfig.currency)
 
-        if ask_for_machine_id:
-            self.addstr(rnstart + 3, 2, "Terminal number:")
-            self.mnfield = ui.editfield(rnstart + 3, 19, 3)
-
-        self.addstr(rnstart + (4 if ask_for_machine_id else 3), 2, " Receipt number:")
-        self.rnfield = ui.editfield(rnstart + (4 if ask_for_machine_id else 3), 19, 16)
-        if cashback_in_use:
-            self.addstr(cbstart, 2, "Is there any cashback?  Enter amount and")
-            self.addstr(cbstart + 1, 2, "press Cash/Enter.  Leave blank and press")
-            self.addstr(cbstart + 2, 2, "Cash/Enter if there is none.")
-            self.addstr(cbstart + 4, 2, "Cashback amount: %s" % tillconfig.currency)
-
-            self.cbfield = ui.moneyfield(cbstart + 4, 19 + len(tillconfig.currency), 6)
-            self._total_line = cbstart + 6
+            self.cbfield = ui.moneyfield(y, 19 + len(tillconfig.currency), 6)
+            fields.append(self.cbfield)
+            y += 2
+            self._total_line = y
+            y += 2
             self.cbfield.sethook = self.update_total_amount
             self.update_total_amount()
-            if cashback_first:
-                firstfield = self.cbfield
-                lastfield = self.rnfield
-            else:
-                firstfield = self.mnfield if ask_for_machine_id else self.rnfield
-                lastfield = self.cbfield
-            fieldlist = [self.rnfield, self.cbfield]
-            if ask_for_machine_id:
-                fieldlist.insert(0, self.mnfield)
-            ui.map_fieldlist(fieldlist)
-        else:
-            self.cbfield = None
-            firstfield = self.mnfield if ask_for_machine_id else self.rnfield
-            lastfield = self.rnfield
-            if ask_for_machine_id:
-                ui.map_fieldlist([self.mnfield, self.rnfield])
-        firstfield.keymap[keyboard.K_CLEAR] = (self.dismiss, None)
-        lastfield.keymap[keyboard.K_CASH] = (self.enter, None)
-        firstfield.focus()
+        y += self.win.wrapstr(
+            y, 2, 40,
+            "Please enter the receipt number from the merchant receipt.")
+        y += 1
+
+        if self.pm._ask_for_machine_id:
+            self.win.addstr(y, 2, "Terminal number:")
+            self.mnfield = ui.editfield(y, 19, 3)
+            fields.append(self.mnfield)
+            y += 1
+
+        self.win.addstr(y, 2, " Receipt number:")
+        self.rnfield = ui.editfield(y, 19, 16)
+        fields.append(self.rnfield)
+        ui.map_fieldlist(fields)
+        fields[0].keymap[keyboard.K_CLEAR] = (self.dismiss, None)
+        fields[-1].keymap[keyboard.K_CASH] = (self.enter, None)
+        fields[0].focus()
 
     def update_total_amount(self):
-        self.addstr(self._total_line, 2, ' ' * 40)
+        self.win.addstr(self._total_line, 2, ' ' * 40)
         try:
             cba = Decimal(self.cbfield.f).quantize(penny)
         except:
             cba = zero
         if cba > self.max_cashback:
-            self.addstr(self._total_line, 2, "Maximum cashback is %s" % (
+            self.win.addstr(self._total_line, 2, "Maximum cashback is %s" % (
                 tillconfig.fc(self.max_cashback)), ui.colour_error)
         else:
-            self.addstr(self._total_line, 2, "Total card payment: %s" % (
-                    tillconfig.fc(self.amount + cba)))
+            self.win.addstr(self._total_line, 2, "Total card payment: %s" % (
+                tillconfig.fc(self.amount + cba)))
 
     def enter(self):
         try:
@@ -104,21 +92,27 @@ class cardpopup(ui.dismisspopup):
                  "transaction.".format(tillconfig.fc(
                      self.max_cashback))],
                 title="Error")
-        receiptno = self.rnfield.f
-        if receiptno == "":
-            return ui.infopopup(["You must enter a receipt number."],
-                                title="Error")
-
-        if self.ask_for_machine_id:
+        ref = []
+        if self.pm._ask_for_machine_id:
             machineno = self.mnfield.f
-            if machineno == "":
+            if machineno == "" and self.pm._ref_required:
+                self.mnfield.focus()
                 return ui.infopopup(["You must enter a card machine id."],
                                     title="Error")
-            ref = "{} {}".format(machineno, receiptno)
-        else:
-            ref = receiptno
+            if machineno:
+                ref.append(machineno)
+
+        receiptno = self.rnfield.f
+        if receiptno == "" and self.pm._ref_required:
+            self.rnfield.focus()
+            return ui.infopopup(["You must enter a receipt number."],
+                                title="Error")
+        if receiptno:
+            ref.append(receiptno)
+
+        ref = " ".join(ref)
         self.dismiss()
-        self.func(self.reg, self.transid, self.amount, cba, ref)
+        self.pm._finish_payment(self.reg, self.transid, self.amount, cba, ref)
 
 
 class BadCashbackMethod(Exception):
@@ -131,22 +125,16 @@ class CardPayment(payment.PaymentMethod):
     refund_supported = True
 
     def __init__(self, paytype, description, machines=1, cashback_method=None,
-                 max_cashback=zero, cashback_first=True, kickout=False,
+                 max_cashback=zero, kickout=False,
                  rollover_guard_time=None,
                  account_code=None, account_date_policy=None,
-                 ask_for_machine_id=False):
+                 ask_for_machine_id=False,
+                 ref_required=True):
         """Accept payments using a manual PDQ machine.
 
         This payment method records the receipt number ("reference")
         from the PDQ machine as confirmation, and optionally supports
         cashback.
-
-        Older versions of the till software had the receipt number
-        field first in the dialog box.  Experience with users showed
-        them filling in the cashback field first and then moving back
-        to the reference field, so now the default is to have the
-        cashback field at the top.  cashback_first can be set to False
-        to restore the old behaviour.
 
         The kickout parameter sets whether the cash drawer will be
         opened on a card transaction that does not include cashback.
@@ -169,15 +157,19 @@ class CardPayment(payment.PaymentMethod):
         payment dialog will ask for a card machine id, which will be
         added to the reference field. This only makes sense if you
         have more than one machine.
+
+        If ref_required is set to False then blank entries for machine
+        ID (if present) and reference will be permitted and will lead
+        to a ref of "" in the database.
         """
-        payment.PaymentMethod.__init__(self, paytype, description)
+        super().__init__(paytype, description)
         self._machines = machines
         self._ask_for_machine_id = ask_for_machine_id if machines > 1 else False
+        self._ref_required = ref_required
         self._cashback_method = cashback_method
         if cashback_method and not cashback_method.change_given:
             raise BadCashbackMethod()
         self._max_cashback = max_cashback
-        self._cashback_first = cashback_first
         self._kickout = kickout
         self._total_fields = [("Terminal {t}".format(t=t + 1),
                                ui.validate_float, None)
@@ -189,8 +181,10 @@ class CardPayment(payment.PaymentMethod):
     def describe_payment(self, payment):
         # Card payments use the 'ref' field for the card receipt number
         if payment.amount >= zero:
-            return "{} {}".format(self.description, payment.ref)
-        return "{} refund {}".format(self.description, payment.ref)
+            return "{}{}{}".format(
+                self.description, " " if payment.ref else "", payment.ref)
+        return "{} refund{}{}".format(
+            self.description, " " if payment.ref else "", payment.ref)
 
     def start_payment(self, reg, transid, amount, outstanding):
         if self._rollover_guard_time:
@@ -226,9 +220,7 @@ class CardPayment(payment.PaymentMethod):
                     ["You can't refund more than the amount due back."],
                     title="Refund too large")
                 return
-            cardpopup(reg, transid, amount, self._finish_payment, zero,
-                      self._cashback_first, self._ask_for_machine_id,
-                      refund=True)
+            _cardpopup(self, reg, transid, amount, zero, refund=True)
             return
         if amount > outstanding:
             if self._cashback_method:
@@ -243,9 +235,8 @@ class CardPayment(payment.PaymentMethod):
                     ["You can't take an overpayment on cards."],
                     title="Overpayment not accepted")
             return
-        cardpopup(reg, transid, amount, self._finish_payment,
-                  self._max_cashback if self._cashback_method else zero,
-                  self._cashback_first, self._ask_for_machine_id)
+        _cardpopup(self, reg, transid, amount,
+                   self._max_cashback if self._cashback_method else zero)
 
     def _finish_payment(self, reg, transid, amount, cashback, ref):
         trans = td.s.query(Transaction).get(transid)
