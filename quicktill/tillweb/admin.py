1b6c893b60a32bc440bf5046bdd8237d9cd2124e
Stephen Early <steve@assorted.org.uk>
2019-11-20 19:07:03 +0000

    Show user access permission in django admin

--- quicktill/tillweb/admin.py
+++ quicktill/tillweb/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 
 def user_full_name(access):
     return access.user.get_full_name()
-user_full_name.short_description="Name"
+user_full_name.short_description = "Name"
 
 class AccessAdmin(admin.ModelAdmin):
-    list_filter=('till',)
-    list_display=('user',user_full_name,'till')
+    list_filter = ('till',)
+    list_display = ('user', user_full_name, 'till', 'permission')
 
 admin.site.register(Till)
-admin.site.register(Access,AccessAdmin)
+admin.site.register(Access, AccessAdmin)
