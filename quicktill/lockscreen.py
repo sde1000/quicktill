d1beedb475656b6a564a454a4b49b775574775b5
Stephen Early <steve@assorted.org.uk>
2020-06-28 19:11:50 +0100

    Extend lockscreen plugin to support regular refresh

--- quicktill/lockscreen.py
+++ quicktill/lockscreen.py
@@ -12,6 +12,18 @@ class LockScreenPlugin(metaclass=plugins.InstancePluginMount):
         """
         pass
 
+    def draw(self, win, y):
+        """Draw on the lock screen; return the new value for 'y'
+        """
+        return y
+
+    def refresh(self):
+        """Return the number of seconds to the next lock screen refresh
+
+        If no refresh is required, return None or 0.
+        """
+        pass
+
 class CheckPrinter(LockScreenPlugin):
     def __init__(self, description, printer):
         self._description = description
@@ -28,6 +40,7 @@ class lockpage(ui.basicpage):
         super().__init__()
         self.win.set_cursor(False)
         self.idle_timeout = None
+        self.refresh_timeout = None
         self.updateheader()
         self._y = 1
         self.line("This till is locked.")
@@ -45,13 +58,20 @@ class lockpage(ui.basicpage):
                 now = time.time()
                 call_at = max(
                     tillconfig.start_time + tillconfig.minimum_run_time,
-                    time.time() + tillconfig.minimum_lock_screen_time)
+                    now + tillconfig.minimum_lock_screen_time)
                 self.idle_timeout = tillconfig.mainloop.add_timeout(
                     call_at - now, self.alarm)
+        refresh_times = []
         for p in LockScreenPlugin.instances:
             l = p.add_note()
             if l:
                 self.line(l)
+            self._y = p.draw(self.win, self._y)
+            refresh_times.append(p.refresh())
+        refresh_time = min((x for x in refresh_times if x), default=0)
+        if refresh_time:
+            self.refresh_timeout = tillconfig.mainloop.add_timeout(
+                refresh_time, self.refresh)
         self.win.wrapstr(
             self.h - 1, 0, self.w, "Till version: {}".format(version.version))
         self.win.move(0, 0)
@@ -70,9 +90,16 @@ class lockpage(ui.basicpage):
                  tillconfig.idle_exit_code)
         tillconfig.mainloop.shutdown(tillconfig.idle_exit_code)
 
+    def refresh(self):
+        # Re-display the page
+        self.refresh_timeout = None
+        self.deselect()
+
     def deselect(self):
         # This page ceases to exist when it disappears.
         super().deselect()
         self.dismiss()
         if self.idle_timeout:
             self.idle_timeout.cancel()
+        if self.refresh_timeout:
+            self.refresh_timeout.cancel()
