2119979df92925605bbe4a299c27bd2b8fb972ae
Stephen Early <steve@assorted.org.uk>
2021-04-28 20:29:07 +0100

    Update Haymakers example config from live installation
    Secret keys removed, of course; also config for online orders,
    timesheets, and lighting control

--- examples/haymakers.py
+++ examples/haymakers.py
@@ -1,31 +1,48 @@
 # -*- coding: utf-8 -*-
 
+# When this module is loaded, 'configname' is set to the configuration
+# name requested on the command line.  The module should define
+# 'configurations' to be a dict indexed by configuration names.
+
 import quicktill.extras
-import quicktill.foodcheck
 from quicktill.keyboard import *
 import quicktill.pdrivers
-import quicktill.register
 import quicktill.ui
 import quicktill.stockterminal
-import quicktill.managetill
-import quicktill.managestock
+import quicktill.register
 import quicktill.stocktype
-import quicktill.pricecheck
 import quicktill.usestock
-import quicktill.recordwaste
-import quicktill.lockscreen
-import quicktill.stock
 import quicktill.cash
 import quicktill.card
-import quicktill.bitcoin
 import quicktill.modifiers
-import quicktill.xero
 import quicktill.localutils
-import quicktill.foodorder
+import quicktill.secretstore
+import quicktill.jsonfoodorder
+import quicktill.xero
 from decimal import Decimal, ROUND_UP
 import datetime
 import logging
 log = logging.getLogger('config')
+
+class ThreeForTwo(quicktill.modifiers.SimpleModifier):
+    """Three for the price of two
+    """
+    def mod_stockline(self, stockline, sale):
+        if not sale.price:
+            raise quicktill.modifiers.Incompatible("No price is set")
+        sale.price = sale.price * Decimal(2)
+        sale.qty = sale.qty * Decimal(3)
+        sale.description = sale.description + " (3 for the price of 2)"
+
+class TwoForOnePointFive(quicktill.modifiers.SimpleModifier):
+    """Two for the price of one-and-a-half
+    """
+    def mod_stockline(self, stockline, sale):
+        if not sale.price:
+            raise quicktill.modifiers.Incompatible("No price is set")
+        sale.price = sale.price * Decimal("1.5")
+        sale.qty = sale.qty * Decimal(2)
+        sale.description = sale.description + " (2 for the price of 1.5)"
 
 class Half(quicktill.modifiers.SimpleModifier):
     """Half pint modifier.
@@ -88,6 +105,24 @@ class Mixer(quicktill.modifiers.SimpleModifier):
         if sale.price:
             sale.price = sale.price / 4
         sale.description = "{} mixer".format(sale.description)
+
+class Case12(quicktill.modifiers.SimpleModifier):
+    """Case of 12 modifier.
+
+    When used with a stockline, checks that the stock is sold in
+    bottles, sets the quantity to 12 and the sale price to 10 times
+    the price of a single bottle.
+    """
+    def mod_stockline(self, stockline, sale):
+        if sale.stocktype.unit.name != 'bottle':
+            raise quicktill.modifiers.Incompatible(
+                "The {} modifier can only be used with bottles."\
+                .format(self.name))
+        if sale.price:
+            sale.price = sale.price * Decimal("10")
+        sale.qty = Decimal("12.0")
+        sale.description = "{} case of 12 bottles".format(
+            sale.stocktype.format())
 
 class Double(quicktill.modifiers.SimpleModifier):
     """Double spirits modifier.
@@ -173,61 +208,49 @@ Wine("Medium", Decimal("175.0"), "175ml glass", "altprice2", Decimal("0.30"))
 Wine("Large", Decimal("250.0"), "250ml glass", "altprice3", Decimal("0.10"))
 Wine("Wine Bottle", Decimal("750.0"), "75cl bottle", "price", Decimal("0.00"))
 
+def gp(stocktype, stockunit, cost, gp):
+    # Calculate a sale price given a cost price and GP
+    # GP is an int or float; 100 is 100%
+    exvat = (cost / (Decimal(1) - Decimal(gp / 100.0))) * stocktype.unit.units_per_item / stockunit.size
+    return stocktype.department.vat.current.exc_to_inc(exvat)\
+        .quantize(Decimal("0.1"), rounding=ROUND_UP)\
+        .quantize(Decimal("0.01"))
+
+def markup(stocktype, stockunit, cost, markup):
+    return stocktype.department.vat.current.exc_to_inc(
+        cost * markup * stocktype.unit.units_per_item / stockunit.size).\
+        quantize(Decimal("0.1"), rounding=ROUND_UP).\
+        quantize(Decimal("0.01"))
+
 # We are passed a StockType (from which we can get manufacturer, name,
 # department, abv and so on); StockUnit (size of container, eg. 72
 # pints) and the ex-VAT cost of that unit
 class PriceGuess(quicktill.stocktype.PriceGuessHook):
+    gp_table = {
+        1: 69, # Real ale
+        2: 69, # Keg
+        3: 69, # Real cider
+        4: 69, # Spirits
+        5: 60, # Snacks
+        6: 69, # Bottles
+        7: 70, # Softs
+        9: 60, # Wine
+    }
+    min_table = {
+        4: Decimal("2.90"),
+    }
     @staticmethod
     def guess_price(stocktype, stockunit, cost):
-        if stocktype.dept_id == 1:
-            return markup(stocktype, stockunit, cost, Decimal("3.0"))
-        if stocktype.dept_id == 3:
-            return markup(stocktype, stockunit, cost, Decimal("2.6"))
-        if stocktype.dept_id == 4:
+        if stocktype.dept_id in PriceGuess.gp_table:
             return max(
-                Decimal("2.50"),
-                markup(stocktype, stockunit, cost, Decimal("2.5")))
-        if stocktype.dept_id == 5:
-            return markup(stocktype, stockunit, cost, Decimal("2.0"))
-        if stocktype.dept_id == 6:
-            return markup(stocktype, stockunit, cost, Decimal("2.5"))
+                gp(stocktype, stockunit, cost,
+                   PriceGuess.gp_table[stocktype.dept_id]),
+                PriceGuess.min_table.get(stocktype.dept_id, Decimal("0.00")))
 
-def markup(stocktype, stockunit, cost, markup):
-    return stocktype.department.vat.current.exc_to_inc(
-        cost * markup / stockunit.size).\
-        quantize(Decimal("0.1"), rounding=ROUND_UP)
-
-# Payment methods.
-cash = quicktill.cash.CashPayment(
-    'CASH', 'Cash', change_description="Change", drawers=2,
-    countup=[], account_code="054")
-def card_expected_payment_date(sessiondate):
-    return quicktill.localutils.delta_england_banking_days(sessiondate, 3)
-card = quicktill.card.CardPayment(
-    'CARD', 'Card', machines=3, cashback_method=cash,
-    max_cashback=Decimal("50.00"), kickout=True,
-    rollover_guard_time=datetime.time(4, 0, 0),
-    account_code="011", account_date_policy=card_expected_payment_date,
-    ask_for_machine_id=False)
-amex = quicktill.card.CardPayment(
-    'AMEX', 'AmEx', machines=3,
-    kickout=True,
-    rollover_guard_time=datetime.time(4, 0, 0),
-    account_code="011", account_date_policy=card_expected_payment_date,
-    ask_for_machine_id=False)
-bitcoin = quicktill.bitcoin.BitcoinPayment(
-    'BTC', 'Bitcoin', site='haymakers', username='haymakers',
-    base_url='http://btcmerch.i.individualpubs.co.uk/merchantservice/',
-    password='not-a-real-password', account_code="641")
-all_payment_methods = [cash, card, amex] # Used for session totals entry
-payment_methods = [cash, card] # Used in register
-
-# Declare discount policies: all simple percentages
 quicktill.register.PercentageDiscount("Staff food", 20, [10])
+quicktill.register.PercentageDiscount("Staff 10% food", 10, [10])
 quicktill.register.PercentageDiscount("Free", 100, permission_required=(
     "convert-to-free-drinks", "Convert a transaction to free drinks"))
-quicktill.register.PercentageDiscount("Test", 10, [1, 2])
-quicktill.register.PercentageDiscount("Half price", 50)
 
 tapi = quicktill.extras.twitter_api(
     token='not-a-real-token',
@@ -254,89 +277,37 @@ class UseStockTwitterHook(quicktill.usestock.UseStockHook):
             quicktill.extras.twitter_post(
                 tapi, default_text=t, fail_silently=True)
 
-import ipltimesheets
-tsapi = ipltimesheets.Api(
-    "haymakers", "not-a-real-password",
-    "https://www.individualpubs.co.uk")
+#UseStockTwitterHook(tapi) - disabled for example, no credentials
 
-try:
-    with open("xero-consumer-key") as f:
-        xero_consumer_key = f.read().strip()
-except:
-    xero_consumer_key = None
-try:
-    with open("xero-private-key.pem") as f:
-        xero_private_key = f.read()
-except:
-    xero_private_key = None
+kitchenprinter = quicktill.pdrivers.nullprinter('kitchen')
 
-# Contact ID and shortcode have been replaced with details from Xero
-# demo company
-#xapi = quicktill.xero.XeroIntegration(
-#    consumer_key=xero_consumer_key,
-#    private_key=xero_private_key,
-#    sales_contact_id="fe196ff8-8b29-4090-b059-380bff6013c5",
-#    reference_template="Haymakers takings session {session.id}",
-#    tracking_category_name="Site",
-#    tracking_category_value="Haymakers",
-#    shortcode="!w3V8N",
-#    discrepancy_account="405",
-#    tillweb_base_url="https://www.individualpubs.co.uk/tillweb/haymakers/",
-#    start_date=datetime.date(2016, 12, 1))
+xapi = quicktill.xero.XeroIntegration(
+    secrets=quicktill.secretstore.Secrets(
+        'xero-test', b'jVHsRc60cDQjTjTnKqyISP41kxKPSeT_kkDvTJjLsaY='))
 
 def appsmenu():
     menu = [
         ("1", "Twitter", quicktill.extras.twitter_client, (tapi,)),
     ]
-    if configname == 'mainbar':
-        menu.append(
-            ("2", "Timesheets", ipltimesheets.popup, (tsapi,)))
-    #menu.append(
-    #    ("4", "Xero integration", xapi.app_menu, ()))
+    menu.append(
+        ("4", "Xero integration", xapi.app_menu, ()))
     quicktill.ui.keymenu(menu, title="Apps")
 
-register_hotkeys = {
-    K_PRICECHECK: quicktill.pricecheck.popup,
-    K_MANAGETILL: quicktill.managetill.popup,
-    K_MANAGESTOCK: quicktill.managestock.popup,
-    K_USESTOCK: quicktill.usestock.popup,
-    K_WASTE: quicktill.recordwaste.popup,
-    K_APPS: appsmenu,
-    's': quicktill.managestock.popup,
-    'S': quicktill.managestock.popup,
-    'a': quicktill.stock.annotate,
-    'A': quicktill.stock.annotate,
-    'r': quicktill.recordwaste.popup,
-    'R': quicktill.recordwaste.popup,
-    't': appsmenu,
-    'T': appsmenu,
-    'm': quicktill.managetill.popup,
-    'M': quicktill.managetill.popup,
-    'l': quicktill.lockscreen.lockpage,
-    'L': quicktill.lockscreen.lockpage,
-}
+# These keys are used by the register and stock terminal pages if they
+# haven't already found a use for a keypress
+register_hotkeys = quicktill.localutils.register_hotkeys(appsmenu=appsmenu)
 
-std = {
-    'pubname': "The Haymakers",
-    'pubnumber': "01223 311077",
-    'pubaddr': ("54 High Street, Chesterton", "Cambridge CB4 1NG"),
-    'currency': "£",
-    'all_payment_methods': all_payment_methods,
-    'payment_methods': payment_methods,
-    'database': 'dbname=haymakers',
-    'checkdigit_print': True,
-    'checkdigit_on_usestock': True,
-}
+global_hotkeys = quicktill.localutils.global_hotkeys(register_hotkeys)
 
-quicktill.foodorder.FoodOrderPlugin(
-    'http://till.haymakers.i.individualpubs.co.uk/foodmenu.py',
-    [quicktill.pdrivers.nullprinter("kitchen")],
-    K_FOODORDER, K_FOODMESSAGE)
+quicktill.jsonfoodorder.FoodOrderPlugin(
+    menuurl='https://www.individualpubs.co.uk/haymakers/menu.json',
+    printers=[kitchenprinter],
+    order_key=K_FOODORDER, message_key=K_FOODMESSAGE,
+    message_department=10, allowable_departments=[10])
 
 noprinter = {
     'printer': quicktill.pdrivers.nullprinter(),
 }
-# Print to a locally-attached receipt printer
 localprinter = {
     'printer': quicktill.pdrivers.autodetect_printer([
         ("/dev/epson-tm-t20",
@@ -349,13 +320,9 @@ localprinter = {
          quicktill.pdrivers.Epson_TM_U220_driver(57, has_cutter=True), False),
         ]),
 }
-pdfprinter = {
-    'printer': quicktill.pdrivers.cupsprinter(
-        "barprinter", driver=quicktill.pdrivers.pdf_driver()),
-}
 xpdfprinter = {
     'printer': quicktill.pdrivers.commandprinter(
-        "evince %s", driver=quicktill.pdrivers.pdf_driver()),
+        "xpdf %s", driver=quicktill.pdrivers.pdf_driver()),
 }
 
 # across, down, width, height, horizgap, vertgap, pagesize
@@ -374,31 +341,53 @@ labelprinter = {
             "DYMO-LabelWriter-450",
             driver=quicktill.pdrivers.pdf_page(pagesize=label99015),
             description="DYMO label printer"),
+        quicktill.pdrivers.commandprinter(
+            "evince %s",
+            driver=quicktill.pdrivers.pdf_labelpage(*staples_3by6)),
     ],
 }
 
-kb1 = {
-    'keyboard': quicktill.localutils.stdkeyboard_16by8(
-        cash_payment_method=cash, card_payment_method=card),
-    'firstpage': quicktill.lockscreen.lockpage,
-    'usertoken_handler': lambda t: quicktill.register.handle_usertoken(
-        t, register_hotkeys, autolock=K_LOCK),
-    'usertoken_listen': ('127.0.0.1', 8455),
-}
+# Payment methods.
+cash = quicktill.cash.CashPayment(
+    'CASH', 'Cash', change_description="Change", drawers=2,
+    countup=[], account_code="054")
+def card_expected_payment_date(sessiondate):
+    # Card payments are expected in the bank account two days after
+    # the session, or the next banking day if the expected date is not
+    # a banking day.
+    date = sessiondate + datetime.timedelta(days=2)
+    while not quicktill.localutils.is_england_banking_day(date):
+        date = date + datetime.timedelta(days=1)
+    return date
+def amex_expected_payment_date(sessiondate):
+    return quicktill.localutils.delta_england_banking_days(sessiondate, 2)
+def sumup_expected_payment_date(sessiondate):
+    # We're not sure yet, but we think sumup payments are in the
+    # account on the next banking day.
+    return quicktill.localutils.delta_england_banking_days(sessiondate, 1)
 
-stock_hotkeys = {
-    's': quicktill.managestock.popup,
-    'S': quicktill.managestock.popup,
-    'a': quicktill.stock.annotate,
-    'A': quicktill.stock.annotate,
-    'r': quicktill.recordwaste.popup,
-    'R': quicktill.recordwaste.popup,
-    't': appsmenu,
-    'T': appsmenu,
-    'm': quicktill.managetill.popup,
-    'M': quicktill.managetill.popup,
-    'l': quicktill.lockscreen.lockpage,
-    'L': quicktill.lockscreen.lockpage,
+card = quicktill.card.CardPayment(
+    'CARD', 'Card', machines=3, cashback_method=cash,
+    max_cashback=Decimal("50.00"), kickout=True,
+    rollover_guard_time=datetime.time(4, 0, 0),
+    account_code="011", account_date_policy=card_expected_payment_date)
+amex = quicktill.card.CardPayment(
+    'AMEX', 'AmEx', machines=3, kickout=True,
+    rollover_guard_time=datetime.time(4, 0, 0),
+    account_code="011", account_date_policy=amex_expected_payment_date)
+sumup = quicktill.card.CardPayment(
+    'SUMUP', 'SumUp', machines=1, kickout=False,
+    account_code="011", account_date_policy=sumup_expected_payment_date)
+
+# Used for session totals entry
+all_payment_methods = [ cash, amex, card ]
+# Used in register
+payment_methods = [ cash, card ]
+
+std = {
+    'all_payment_methods': all_payment_methods,
+    'payment_methods': payment_methods,
+    'database': 'dbname=haymakers',
 }
 
 global_hotkeys = {
@@ -409,14 +398,7 @@ global_hotkeys = {
 
 stockcontrol = {
     'firstpage': lambda: quicktill.stockterminal.page(
-        stock_hotkeys, ["Bar"]),
-}
-
-stockcontrol_terminal = {
-    'firstpage': quicktill.lockscreen.lockpage,
-    'usertoken_handler': lambda t: quicktill.stockterminal.handle_usertoken(
-        t, register_hotkeys, ["Bar"], max_unattended_updates=5),
-    'usertoken_listen': ('127.0.0.1', 8455),
+        register_hotkeys, ["Bar"]),
 }
 
 config0 = {'description': "Stock-control terminal, no user"}
@@ -425,31 +407,43 @@ config0.update(stockcontrol)
 config0.update(xpdfprinter)
 config0.update(labelprinter)
 
-config1 = {'description': "Haymakers main bar",
-           'hotkeys': global_hotkeys}
+config1 = {
+    'description': "Haymakers main bar",
+    'hotkeys': global_hotkeys,
+    'keyboard': quicktill.localutils.stdkeyboard_16by8(
+        cash_payment_method=cash, card_payment_method=card),
+}
 config1.update(std)
-config1.update(kb1)
+config1.update(quicktill.localutils.activate_register_with_usertoken(
+    register_hotkeys))
 config1.update(localprinter)
 config1.update(labelprinter)
 
-config2 = {'description':"Stock-control terminal, card reader"}
+config2 = {
+    'description': "Stock-control terminal, card reader",
+}
 config2.update(std)
-config2.update(stockcontrol_terminal)
+config2.update(quicktill.localutils.activate_stockterminal_with_usertoken(
+    register_hotkeys))
 config2.update(xpdfprinter)
 config2.update(labelprinter)
 
-config4 = {'description': "Haymakers festival bar",
-           'hotkeys': global_hotkeys}
+config4 = {
+    'description': "Haymakers festival bar",
+    'hotkeys': global_hotkeys,
+    'keyboard': quicktill.localutils.stdkeyboard_20by7(
+        151, cash_payment_method=cash, card_payment_method=card),
+}
 config4.update(std)
-config4.update(kb1)
-config4['keyboard'] = quicktill.localutils.stdkeyboard_20by7(
-    151, cash_payment_method=cash, card_payment_method=card)
+config4.update(quicktill.localutils.activate_register_with_usertoken(
+    register_hotkeys))
 config4.update(localprinter)
 config4.update(labelprinter)
 
 configurations = {
     'default': config0,
     'mainbar': config1,
+    'secondbar': config1,
     'stockterminal': config2,
     'festival': config4,
 }
