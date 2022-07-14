0073cc196ac4aeed96c4885102702fe730f257d1
Stephen Early <steve@assorted.org.uk>
2022-07-14 12:07:37 +0100

    Change from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,36 +2,4 @@
 
 from setuptools import setup
 
-setup(name='quicktill',
-      version='20.2',
-      description='Quick till and stock control library',
-      author='Stephen Early',
-      author_email='steve@assorted.org.uk',
-      url='https://github.com/sde1000/quicktill',
-      packages=['quicktill', 'quicktill.tillweb',
-                'quicktill.tillweb.migrations'],
-      package_data={'quicktill.tillweb':
-                    ['static/tillweb/*.js',
-                     'static/tillweb/*.css',
-                     'static/tillweb/multi-select/*/*',
-                     'static/tillweb/select2/*',
-                     'templates/tillweb/*.html',
-                     'templates/tillweb/*.ajax',
-                    ]},
-      scripts=['runtill'],
-      install_requires=[
-          "psycopg2",
-          "reportlab",
-          "httplib2",
-          "sqlalchemy",
-          "qrcode",
-          "pyyaml",
-          "odfpy",
-          "twython",
-          "requests",
-          "requests-oauthlib",
-          "pycups",
-          "python-dateutil",
-          "cryptography",
-      ],
-)
+setup()
