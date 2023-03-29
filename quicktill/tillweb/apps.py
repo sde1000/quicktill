4115c04c22d3f64cffebc4222061648212de6777
Stephen Early <steve@assorted.org.uk>
2023-03-29 13:18:28 +0100

    Add apps module to tillweb

---
+++ quicktill/tillweb/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+from django.urls import reverse
+
+
+class TillWebConfig(AppConfig):
+    name = 'quicktill.tillweb'
+    verbose_name = "Till web interface"
+
+    def ready(self):
+        self.Till = self.get_model("Till")
+
+    def get_sidebar(self, user):
+        tills = self.Till.objects.filter(access__user=user).order_by('name')
+        if len(tills) > 0:
+            return [{'name': "Till access",
+                     'objects': tills}]
+        return []
