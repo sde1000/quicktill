9e335cc9000212eacc3d527a35e2a20156c3c66c
James Addison <jay@jp-hosting.net>
2024-05-07 01:22:01 +0100

    Padding: expand each pad-bit to a pad-byte.

--- quicktill/pdrivers.py
+++ quicktill/pdrivers.py
@@ -724,7 +724,7 @@ class escpos:
 
         # Calculate padding required to center the image
         padding = (self.dpl - width) // 2
-        padchars = [False] * padding
+        padchars = [False] * padding * 8
 
         # Partition the bitmap into lines
         lines = []
