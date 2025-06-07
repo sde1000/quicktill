4c60ff6428d8a5eb3bde6cf8355e0eaaa9e79a86
Stephen Early <steve@assorted.org.uk>
2025-06-07 16:57:44 +0100

    PEP8 on a couple of very old example files

--- examples/examplemenu.py
+++ examples/examplemenu.py
@@ -1,195 +1,219 @@
-from quicktill.foodorder import simplemenu,subopts
-from quicktill.models import zero,penny
-from decimal import Decimal,ROUND_UP
+from quicktill.foodorder import simplemenu, subopts
+from quicktill.models import zero, penny
+from decimal import Decimal, ROUND_UP
 
-footer="This is a footer"
-dept=10
+footer = "This is a footer"
+dept = 10
 
-def staffdiscount(tablenumber,item):
-    if tablenumber!=1234: return zero
-    discount=item.price*Decimal("0.4")
-    if discount>Decimal("3.00"): discount=Decimal("3.00")
-    discount=discount.quantize(Decimal("0.1"),rounding=ROUND_UP)
-    discount=discount.quantize(penny)
+
+def staffdiscount(tablenumber, item):
+    if tablenumber != 1234:
+        return zero
+    discount = item.price * Decimal("0.4")
+    if discount > Decimal("3.00"):
+        discount = Decimal("3.00")
+    discount = discount.quantize(Decimal("0.1"), rounding=ROUND_UP)
+    discount = discount.quantize(penny)
     return discount
 
+
 class ices(subopts):
-    """
-    A special version of the subopts class that understands how
+    """A special version of the subopts class that understands how
     ice-cream is priced.  It's very simple: ice cream costs 3 pounds
     for two scoops, and 1 pound for every extra scoop.
-
     """
-    def __init__(self,options):
+    def __init__(self, options):
         # We initialise the subopts class with zero prices for the
         # item and all its suboptions; this is because we override the
         # "price" method below.
-        subopts.__init__(self,"Ice Cream / Sorbet",0.00,
-                         [(x,0.00) for x in options],
-                         atleast=1,nameconnector=": ",
+        super().__init__("Ice Cream / Sorbet", 0.00,
+                         [(x, 0.00) for x in options],
+                         atleast=1, nameconnector=": ",
                          connector=", ")
-    def price(self,chosen_options):
+
+    def price(self, chosen_options):
         # Find out how many scoops were entered.
-        scoops=len(chosen_options)
+        scoops = len(chosen_options)
         # All scoops after number 2 are considered "extra" - how many of
         # these are there?  The max() function is used to make sure we
         # don't end up with a negative number of extra scoops!
-        extrascoops=max(scoops-2,0)
+        extrascoops = max(scoops - 2, 0)
         # Finally, calculate the price
-        return 3.00+(1.00*extrascoops)
+        return 3.00 + (1.00 * extrascoops)
+
 
 # A convenience function that saves some typing for the common case where
 # you want to specify some options for a dish that don't affect its price.
 # "options" is a list of strings.
-def with_options(name,price,options):
+def with_options(name, price, options):
     return (name,
-            subopts(name,price,[(x,0.00) for x in options],
-                    connector=", ",nameconnector=" "))
+            subopts(name, price, [(x, 0.00) for x in options],
+                    connector=", ", nameconnector=" "))
 
-lunchPloughmans=simplemenu([
-    ("Lunch menu: Cheddar Ploughmans",4.20),
-    ("Lunch menu: Stilton Ploughmans",4.20)],title="Ploughmans")
 
-lunchOmelette=simplemenu([
-    ("Lunch menu: Cheese Omelette",4.20),
-    ("Lunch menu: Tomato Omelette",4.20),
-    ("Lunch menu: Mushroom Omelette",4.20)],title="Omelettes")
+lunchPloughmans = simplemenu(
+    [
+        ("Lunch menu: Cheddar Ploughmans", 4.20),
+        ("Lunch menu: Stilton Ploughmans", 4.20),
+    ], title="Ploughmans")
 
-lunchmenu=simplemenu([
-    ("Lunch menu: Soup",3.50),
-    ("Lunch menu: Pork Pie",4.20),
-    ("Lunch menu: Salmon and Egg",4.20),
-    ("Lunch menu: Beef Burger",4.20),
-    ("Lunch menu: Ploughman's",lunchPloughmans),
-    ("Lunch menu: Sausages",4.20),
-    ("Lunch menu: Eggs Benedict",4.20),
-    ("Lunch menu: Chicken Salad",4.20),
-    ("Lunch menu: Omelette",lunchOmelette),
-    ],title="Lunch menu")
+lunchOmelette = simplemenu(
+    [
+        ("Lunch menu: Cheese Omelette", 4.20),
+        ("Lunch menu: Tomato Omelette", 4.20),
+        ("Lunch menu: Mushroom Omelette", 4.20),
+    ], title="Omelettes")
 
-starters=simplemenu([
-    ("Carrot & Coriander Soup",3.50),
-    ("Pork Pie and Salad",4.50),
-    ("Smoked Trout Salad",4.50),
-    ("Chicken Pasty",4.50),
-    ("Chorizo & Artichoke Salad",4.50)
-    ],title="Starters")
+lunchmenu = simplemenu(
+    [
+        ("Lunch menu: Soup", 3.50),
+        ("Lunch menu: Pork Pie", 4.20),
+        ("Lunch menu: Salmon and Egg", 4.20),
+        ("Lunch menu: Beef Burger", 4.20),
+        ("Lunch menu: Ploughman's", lunchPloughmans),
+        ("Lunch menu: Sausages", 4.20),
+        ("Lunch menu: Eggs Benedict", 4.20),
+        ("Lunch menu: Chicken Salad", 4.20),
+        ("Lunch menu: Omelette", lunchOmelette),
+    ], title="Lunch menu")
 
-steak=simplemenu([
-    ("Blue Steak",10.50),
-    ("Rare Steak",10.50),
-    ("Medium Rare Steak",10.50),
-    ("Medium Steak",10.50),
-    ("Medium Well Done Steak",10.50),
-    ("Well Done Steak",10.50),
-    ("Charcoal Steak",10.50)],title="Steak")
+starters = simplemenu(
+    [
+        ("Carrot & Coriander Soup", 3.50),
+        ("Pork Pie and Salad", 4.50),
+        ("Smoked Trout Salad", 4.50),
+        ("Chicken Pasty", 4.50),
+        ("Chorizo & Artichoke Salad", 4.50),
+    ], title="Starters")
 
-sausages=subopts("Sausages",7.50,
-                 [("Cumberland",0.00),
-                  ("Pork & Leek",0.00),
-                  ("Boerwars",0.00)],
-                 atmost=3, atleast=1, connector=", ")
+steak = simplemenu(
+    [
+        ("Blue Steak", 10.50),
+        ("Rare Steak", 10.50),
+        ("Medium Rare Steak", 10.50),
+        ("Medium Steak", 10.50),
+        ("Medium Well Done Steak", 10.50),
+        ("Well Done Steak", 10.50),
+        ("Charcoal Steak", 10.50),
+    ], title="Steak")
 
-maincourses=simplemenu([
-    with_options("Ploughman's",7.50,
-                 ["without Ardrahan","without Stilton","without Golden Cross",
-                  "without Cheddar",
-                  "with Ardrahan instead","with Stilton instead",
-                  "with Golden Cross instead","with Cheddar instead",
-                  "filler","filler 2","filler 3","filler 4","filler 5",
-                  "filler 6"]),
-    ("Nut Burger",7.50),
-    ("Sausages",sausages),
-    ("Beef Burger",7.50),
-    ("Lamb & Apricot Stew",7.50),
-    ("Tagliatelle",7.50),
-    ("Aubergine Bake",7.50),
-    ("Beef & Rauchbier Pie",8.50),
-    with_options("Grey Mullet Fillet",8.50,["with chips instead"]),
-    ("Steak",steak)],title="Main courses")
+sausages = subopts(
+    "Sausages", 7.50,
+    [
+        ("Cumberland", 0.00),
+        ("Pork & Leek", 0.00),
+        ("Boerwars", 0.00),
+    ], atmost=3, atleast=1, connector=", ")
 
-snacks=simplemenu([
-    ("Bowl of chips",2.50),
-    ("Bowl of olives",1.50)
-    ],title="Snacks")
+maincourses = simplemenu(
+    [
+        with_options(
+            "Ploughman's", 7.50,
+            ["without Ardrahan", "without Stilton", "without Golden Cross",
+             "without Cheddar", "with Ardrahan instead", "with Stilton instead",
+             "with Golden Cross instead", "with Cheddar instead",
+             "filler", "filler 2", "filler 3", "filler 4", "filler 5",
+             "filler 6"]),
+        ("Nut Burger", 7.50),
+        ("Sausages", sausages),
+        ("Beef Burger", 7.50),
+        ("Lamb & Apricot Stew", 7.50),
+        ("Tagliatelle", 7.50),
+        ("Aubergine Bake", 7.50),
+        ("Beef & Rauchbier Pie", 8.50),
+        with_options("Grey Mullet Fillet", 8.50, ["with chips instead"]),
+        ("Steak", steak),
+    ], title="Main courses")
+
+snacks = simplemenu(
+    [
+        ("Bowl of chips", 2.50),
+        ("Bowl of olives", 1.50),
+    ], title="Snacks")
 
 # This makes use of the "ices" class defined at the top of this file.
-icecream=ices(["Vanilla","Chocolate","Strawberry","Chilli",
-               "Lemon","Mango","Blackcurrant"])
+icecream = ices(["Vanilla", "Chocolate", "Strawberry", "Chilli",
+                 "Lemon", "Mango", "Blackcurrant"])
 
-desserts=simplemenu([
-    ("Fruit Salad",3.50),
-    ("Ice Cream",icecream),
-    ("Banana and Toffee Pancakes",4.50),
-    ("Chocolate Fudge Cake",4.50),
-    ("Treacle Sponge",4.50),
-    ("Apple Pie",4.50),
-    ("Apple & Blackberry Pie",4.50),
-    ("Cheese & Biscuits",7.50)
-    ],title="Desserts")
-    
+desserts = simplemenu(
+    [
+        ("Fruit Salad", 3.50),
+        ("Ice Cream", icecream),
+        ("Banana and Toffee Pancakes", 4.50),
+        ("Chocolate Fudge Cake", 4.50),
+        ("Treacle Sponge", 4.50),
+        ("Apple Pie", 4.50),
+        ("Apple & Blackberry Pie", 4.50),
+        ("Cheese & Biscuits", 7.50),
+    ], title="Desserts")
+
 # We're going to do something special here so that we don't have to
 # type in the list of fillings twice.
-sandwichfillings=[
-    "Tuna Mayo","Ham & Mustard","Cheddar & Onion",
-    "Stilton","Roast Chicken","Sausage","BLT"]
+sandwichfillings = [
+    "Tuna Mayo", "Ham & Mustard", "Cheddar & Onion",
+    "Stilton", "Roast Chicken", "Sausage", "BLT"]
 
-sandwiches=simplemenu([
-    ("White bread",
-     simplemenu([("%s on white bread"%filling,3.50)
-                 for filling in sandwichfillings],
-                title="Sandwiches")),
-    ("Malted grain bread",
-     simplemenu([("%s on malted grain bread"%filling,3.50)
-                 for filling in sandwichfillings],
-                title="Sandwiches")),
-    ],title="Sandwiches")
+sandwiches = simplemenu(
+    [
+        ("White bread",
+         simplemenu([("%s on white bread" % filling, 3.50)
+                     for filling in sandwichfillings],
+                    title="Sandwiches")),
+        ("Malted grain bread",
+         simplemenu([("%s on malted grain bread" % filling, 3.50)
+                     for filling in sandwichfillings],
+                    title="Sandwiches")),
+    ], title="Sandwiches")
 
-jackets=subopts("Jacket potato",3.25,
-                [("Tuna Mayo",0.75),
-                 ("Bacon",0.75),
-                 ("Stilton",0.75),
-                 ("Cheddar",0.75),
-                 ("Ham",0.75),
-                 ("Baked Beans",0.75),
-                 ("More",[("More 1",0.01),("More 2",0.02)])],
-                connector=", ",
-                nameconnector=" with ",atleast=1)
+jackets = subopts(
+    "Jacket potato", 3.25,
+    [
+        ("Tuna Mayo", 0.75),
+        ("Bacon", 0.75),
+        ("Stilton", 0.75),
+        ("Cheddar", 0.75),
+        ("Ham", 0.75),
+        ("Baked Beans", 0.75),
+        ("More", [("More 1", 0.01), ("More 2", 0.02)]),
+    ], connector=", ", nameconnector=" with ", atleast=1)
 
-courses=simplemenu([
-    ("First course...",0.00),
-    ("Second course...",0.00),
-    ("Third course...",0.00),
-    ("More to come...",0.00),
-    ("Special instructions",0.00),
-    # Extra options to test that this is correctly spilled into a menu
-    # rather than a keymenu when we run out of numeric keypad keys:
-    ("Six...",0.00),
-    ("Seven...",0.00),
-    ("Eight...",0.00),
-    ("Nine...",0.00),
-    ("Ten...",0.00),
-    ("Eleven...",0.00),
-    ("Twelve...",0.00),
-])
+courses = simplemenu(
+    [
+        ("First course...", 0.00),
+        ("Second course...", 0.00),
+        ("Third course...", 0.00),
+        ("More to come...", 0.00),
+        ("Special instructions", 0.00),
+        # Extra options to test that this is correctly spilled into a menu
+        # rather than a keymenu when we run out of numeric keypad keys:
+        ("Six...", 0.00),
+        ("Seven...", 0.00),
+        ("Eight...", 0.00),
+        ("Nine...", 0.00),
+        ("Ten...", 0.00),
+        ("Eleven...", 0.00),
+        ("Twelve...", 0.00),
+    ])
 
-roast=simplemenu([
-    ("Roast Chicken",7.50),
-    ("Roast Lamb",7.50),
-    ("Nut Roast",7.50)],title="Sunday Roast")
+roast = simplemenu(
+    [
+        ("Roast Chicken", 7.50),
+        ("Roast Lamb", 7.50),
+        ("Nut Roast", 7.50),
+    ], title="Sunday Roast")
 
-menu=[
-    ('Lunch menu',lunchmenu),
-    ('Starters',starters),
-    ('Main courses',maincourses),
-#    ('Sunday Roast',roast),
-    ('Snacks',snacks),
-    ('Desserts',desserts),
-    ('Sandwiches',sandwiches),
-    ('Jacket potatos',jackets),
-    ('Courses / misc',courses),
-    ("more",0.00),
-    ("more",0.00),
-    ("more",0.00),
-    ("more",0.00),
-    ]
+menu = [
+    ('Lunch menu', lunchmenu),
+    ('Starters', starters),
+    ('Main courses', maincourses),
+    # ('Sunday Roast', roast),
+    ('Snacks', snacks),
+    ('Desserts', desserts),
+    ('Sandwiches', sandwiches),
+    ('Jacket potatos', jackets),
+    ('Courses / misc', courses),
+    ("more", 0.00),
+    ("more", 0.00),
+    ("more", 0.00),
+    ("more", 0.00),
+]
--- examples/keycaps.py
+++ examples/keycaps.py
@@ -1,98 +1,105 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 # Example script to generate a grid of keyboard labels suitable for
 # cutting out and putting under the caps of a Preh keyboard
 
-from __future__ import unicode_literals
-
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4 as A4
 
-pw,ph=A4
+pw, ph = A4
 
 # Buttons come in single and double types
-bs=(39,92)
+bs = (39, 92)
 
-class button(object):
+
+class button:
     def __init__(self):
-        self.width=bs[0]
-        self.height=bs[0]
-    def draw(self,f):
+        self.width = bs[0]
+        self.height = bs[0]
+
+    def draw(self, f):
         f.setLineWidth(0.25)
-        f.rect(0,0,self.width,self.height)
+        f.rect(0, 0, self.width, self.height)
+
 
 class textbutton(button):
-    def __init__(self,lines):
-        button.__init__(self)
-        self.lines=lines
+    def __init__(self, lines):
+        super().__init__()
+        self.lines = lines
         self.lines.reverse()
-        self.fontsize=8
-        self.pitch=10
-        self.font="Helvetica"
-    def draw(self,f):
-        button.draw(self,f)
-        f.setFont(self.font,self.fontsize)
-        l=len(self.lines)
-        totalheight=(l-1)*self.pitch+self.fontsize
-        y=(self.height-totalheight)/2+1
+        self.fontsize = 8
+        self.pitch = 10
+        self.font = "Helvetica"
+
+    def draw(self, f):
+        button.draw(self, f)
+        f.setFont(self.font, self.fontsize)
+        l = len(self.lines)
+        totalheight = (l - 1) * self.pitch + self.fontsize
+        y = (self.height - totalheight) / 2 + 1
         for i in self.lines:
-            f.drawCentredString(self.width/2,y,i)
-            y=y+self.pitch
+            f.drawCentredString(self.width / 2, y, i)
+            y = y + self.pitch
+
 
 class ss(textbutton):
-    def __init__(self,*args):
-        textbutton.__init__(self,list(args))
+    def __init__(self, *args):
+        super().__init__(list(args))
+
 
 class bss(textbutton):
-    def __init__(self,text):
-        textbutton.__init__(self,[text])
-        self.fontsize=20
-        self.font="Helvetica-Bold"
+    def __init__(self, text):
+        super().__init__([text])
+        self.fontsize = 20
+        self.font = "Helvetica-Bold"
+
 
 class dd(ss):
-    def __init__(self,*args):
-        ss.__init__(self,*args)
-        self.width=bs[1]
-        self.height=bs[1]
-        self.fontsize=18
-        self.pitch=18
-        self.font="Helvetica-Bold"
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.width = bs[1]
+        self.height = bs[1]
+        self.fontsize = 18
+        self.pitch = 18
+        self.font = "Helvetica-Bold"
+
 
 class ds(textbutton):
-    def __init__(self,text):
-        textbutton.__init__(self,[text])
-        self.width=bs[1]
-        self.fontsize=16
+    def __init__(self, text):
+        super().__init__([text])
+        self.width = bs[1]
+        self.fontsize = 16
+
 
 class arrow(button):
-    def __init__(self,kind):
-        button.__init__(self)
-        self.kind=kind
-    def draw(self,f):
-        button.draw(self,f)
-        f.translate(self.width/2,self.height/2)
-        if self.kind=='LEFT':
+    def __init__(self, kind):
+        super().__init__()
+        self.kind = kind
+
+    def draw(self, f):
+        button.draw(self, f)
+        f.translate(self.width / 2, self.height / 2)
+        if self.kind == 'LEFT':
             f.rotate(90)
-        elif self.kind=='RIGHT':
+        elif self.kind == 'RIGHT':
             f.rotate(270)
-        elif self.kind=='DOWN':
+        elif self.kind == 'DOWN':
             f.rotate(180)
-        p=f.beginPath()
-        p.moveTo(-1,0)
-        p.lineTo(-1,10)
-        p.lineTo(-3,10)
-        p.lineTo(0,13)
-        p.lineTo(3,10)
-        p.lineTo(1,10)
-        p.lineTo(1,0)
+        p = f.beginPath()
+        p.moveTo(-1, 0)
+        p.lineTo(-1, 10)
+        p.lineTo(-3, 10)
+        p.lineTo(0, 13)
+        p.lineTo(3, 10)
+        p.lineTo(1, 10)
+        p.lineTo(1, 0)
         p.close()
-        f.drawPath(p,stroke=0,fill=1)
-    
-    
+        f.drawPath(p, stroke=0, fill=1)
+
+
 # A list of keycaps on the standard IPL keyboard
 
-kl=(
+kl = (
     # Bright yellow
     ss("Alice"),
     ss("Bob"),
@@ -106,27 +113,27 @@ kl=(
     ss("£10"),
     ss("£5"),
     ds("CARD"),
-    dd("CASH","/ ENTER"),
+    dd("CASH", "/ ENTER"),
     # Bright red
     ss("Clear"),
     ds("Lock"),
     # Green
-    ss("Stock","Terminal"),
-    ss("Recall","Trans"),
-    ss("Price","Check"),
+    ss("Stock", "Terminal"),
+    ss("Recall", "Trans"),
+    ss("Price", "Check"),
     ss("Cancel"),
-    ss("Manage","Till"),
-    ss("Manage","Stock"),
-    ss("Use","Stock"),
-    ss("Record","Waste"),
-    ss("Manage","Trans"),
+    ss("Manage", "Till"),
+    ss("Manage", "Stock"),
+    ss("Use", "Stock"),
+    ss("Record", "Waste"),
+    ss("Manage", "Trans"),
     ss("Print"),
     ss("Apps"),
-    ss("Order","Food"),
-    ss("Kitchen","Message"),
+    ss("Order", "Food"),
+    ss("Kitchen", "Message"),
     # Dark blue - departments
     ss("Misc"),
-    ss("Hot","Drinks"),
+    ss("Hot", "Drinks"),
     ss("Wine"),
     ss("Soft"),
     ss("Food"),
@@ -153,7 +160,7 @@ kl=(
     # Dark blue - same as departments
     ss("Crisps"),
     ss("Nuts"),
-    ss("Other","Snacks"),
+    ss("Other", "Snacks"),
     ss("Rolls"),
     ss("Pint 1"),
     ss("Pint 2"),
@@ -163,9 +170,9 @@ kl=(
     ss("Half 6"),
     ss("Half 7"),
     ss("Half 8"),
-    ss("Pint","Lager"),
-    ss("Pint","Thatchers"),
-    ss("Pint","Real Cider"),
+    ss("Pint", "Lager"),
+    ss("Pint", "Thatchers"),
+    ss("Pint", "Real Cider"),
 
     # Pale blue
     ss("Half 1"),
@@ -176,9 +183,9 @@ kl=(
     ss("Pint 6"),
     ss("Pint 7"),
     ss("Pint 8"),
-    ss("Half","Lager"),
-    ss("Half","Thatchers"),
-    ss("Half","Real Cider"),
+    ss("Half", "Lager"),
+    ss("Half", "Thatchers"),
+    ss("Half", "Real Cider"),
 
     # Bright yellow
     ss("Coke Can"),
@@ -186,30 +193,30 @@ kl=(
     ss("J2O"),
 
     # Orangey
-    ss("Fruit","Beer"),
+    ss("Fruit", "Beer"),
     ss("Trappist"),
-    ss("Cider / ","Ginger"),
-    ss("German","Bottle"),
+    ss("Cider / ", "Ginger"),
+    ss("German", "Bottle"),
     ss("Abbaye"),
-    ss("Craft","Bottle"),
-    ss("Other","Bottle"),
-    ss("Soft","Bottle"),
+    ss("Craft", "Bottle"),
+    ss("Other", "Bottle"),
+    ss("Soft", "Bottle"),
 
     # Pale green
-    ss("Bar","Spirits"),
+    ss("Bar", "Spirits"),
     ss("Bells"),
-    ss("Southern","Comfort"),
+    ss("Southern", "Comfort"),
     ss("Bacardi"),
     ss("Brandy"),
     ss("Lambs"),
     ss("Smirnoff"),
     ss("Gordons"),
-    ss("Half","Soft Drink"),
-    ss("Pint","Soft Drink"),
-    ss("Dash /","Mixer"),
-    ss("125ml","Wine"),
-    ss("175ml","Wine"),
-    ss("250ml","Wine"),
+    ss("Half", "Soft Drink"),
+    ss("Pint", "Soft Drink"),
+    ss("Dash /", "Mixer"),
+    ss("125ml", "Wine"),
+    ss("175ml", "Wine"),
+    ss("250ml", "Wine"),
 
     # Pale red
     ss("Gins"),
@@ -217,32 +224,31 @@ kl=(
     ss("Islay"),
     ss("Lowland"),
     ss("Highland"),
-    ss("Other","Spirit"),
+    ss("Other", "Spirit"),
 
     # White
-    ss("Half","Garage"),
-    ss("Pint","Garage"),
-    )
+    ss("Half", "Garage"),
+    ss("Pint", "Garage"),
+)
 
-if __name__=="__main__":
-    f=canvas.Canvas("caps.pdf")
-    margin=28
-    maxx=pw-margin
-    x=margin
-    y=margin
-    lh=0
+if __name__ == "__main__":
+    f = canvas.Canvas("caps.pdf")
+    margin = 28
+    maxx = pw - margin
+    x = margin
+    y = margin
+    lh = 0
     for i in kl:
-        if (x+i.width)>maxx:
-            x=margin
-            y=y+lh
-            lh=0
+        if (x + i.width) > maxx:
+            x = margin
+            y = y + lh
+            lh = 0
         f.saveState()
-        f.translate(x,y)
+        f.translate(x, y)
         i.draw(f)
         f.restoreState()
-        lh=max(lh,i.height)
-        x=x+i.width
-        
+        lh = max(lh, i.height)
+        x = x + i.width
+
     f.showPage()
     f.save()
-    del f
