ff42e5a1a0d79d727cff6cc45f594e79e5e9147d
Stephen Early <steve@assorted.org.uk>
2026-02-15 21:31:22 +0000

    Merge dbsetup into dbutils
    dbsetup used to be a lot larger, when it had an embedded template. Now
    the template is under examples/ and dbsetup is just a small command-line
    class.

--- quicktill/dbutils.py
+++ quicktill/dbutils.py
@@ -3,6 +3,7 @@
 
 import os
 import argparse
+import tomli
 from . import cmdline
 from . import td
 from . import models
@@ -86,6 +87,37 @@ class flushdb(cmdline.command):
                 return 1
         td.remove_tables()
         print("Finished.")
+
+
+class dbsetup(cmdline.command):
+    """Add initial records to the database
+    """
+    help = "add initial records to the database"
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument("dbfile", help="Initial records file "
+                            "in TOML", type=argparse.FileType('rb'))
+
+    @staticmethod
+    def run(args):
+        t = tomli.load(args.dbfile)
+
+        with td.orm_session():
+            try:
+                # Keys in the file are database model class names,
+                # values are lists of dicts giving initial data for
+                # rows of these
+                for modelname, rows in t.items():
+                    model = models.__dict__.get(modelname)
+                    if not model:
+                        raise Exception(f"Unknown model {modelname}")
+                    for row in rows:
+                        td.s.merge(model(**row))
+                        td.s.flush()
+            except Exception:
+                td.s.rollback()
+                raise
 
 
 class anonymise(cmdline.command):
